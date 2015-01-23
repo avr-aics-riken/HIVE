3ffdddbf7a0e9596d212df3c9eb909096cdd01ca
kioku <kentaro-o@digirea.com>
2015-01-23 20:33:54 +0900

    add Type

--- hrender/src/RenderObject/RenderObject.h
+++ hrender/src/RenderObject/RenderObject.h
@@ -10,8 +10,17 @@
 class RenderObject : public RefCount
 {
 public:
+    enum MODE_TYPE {
+        TYPE_CAMERA,
+        TYPE_POLYGON,
+        TYPE_VOLUME,
+        TYPE_POINT,
+    };
+    
     RenderObject(){  };
     ~RenderObject(){};
+    
+    MODE_TYPE GetType() { return TYPE_POLYGON; };
 };
 
 #endif //_RENDEROBJECT_H_
