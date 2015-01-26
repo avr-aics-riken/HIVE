ebdf7fae2b44390ca4f95ce4aacbd925202d6309
masakisasaki <masaki-s@digirea.com>
2015-01-26 11:56:54 +0900

    Update RenderObject and add native PolygonModel.

--- hrender/src/RenderObject/RenderObject.h
+++ hrender/src/RenderObject/RenderObject.h
@@ -1,26 +1,36 @@
 /*
-    RenderObject.h
-*/
+	 RenderObject.h
+ */
 #ifndef _RENDEROBJECT_H_
 #define _RENDEROBJECT_H_
 
 
 #include "../Core/Ref.h"
+#include "BufferMeshData.h"
 
 class RenderObject : public RefCount
 {
 public:
-    enum MODE_TYPE {
-        TYPE_CAMERA,
-        TYPE_POLYGON,
-        TYPE_VOLUME,
-        TYPE_POINT,
-    };
-    
-    RenderObject(){  };
-    ~RenderObject(){};
-    
-    MODE_TYPE GetType() { return TYPE_POLYGON; };
+	enum MODE_TYPE {
+		TYPE_NONE,
+		TYPE_SETTING,
+		TYPE_CAMERA,
+		TYPE_VOLUME,
+		TYPE_POLYGON,
+		TYPE_POINT,
+		TYPE_MAX,
+	};
+
+	RenderObject(){  };
+	~RenderObject(){};
+	MODE_TYPE GetType()    { return type; }
+	BufferMeshData &Mesh() { return mesh; }
+	void SetType(MODE_TYPE t)        { type = t; }
+	void SetMesh(BufferMeshData &m)  { mesh = m; }
+private:
+	MODE_TYPE      type;
+	BufferMeshData mesh;
+protected:
 };
 
 #endif //_RENDEROBJECT_H_
