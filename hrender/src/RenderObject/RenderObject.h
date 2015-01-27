0dc1286e2e2c78e72dcd275476dbcddb19e88459
masakisasaki <masaki-s@digirea.com>
2015-01-27 17:17:53 +0900

    Remove unuse definition

--- hrender/src/RenderObject/RenderObject.h
+++ hrender/src/RenderObject/RenderObject.h
@@ -4,36 +4,29 @@
 #ifndef _RENDEROBJECT_H_
 #define _RENDEROBJECT_H_
 
-
 #include "../Core/Ref.h"
-//#include "BufferMeshData.h"
-
-class BufferMeshData;
-class CameraData;
 
 class RenderObject : public RefCount
 {
 public:
-	enum MODE_TYPE {
-		TYPE_NONE,
-		TYPE_SETTING,
-		TYPE_CAMERA,
-		TYPE_VOLUME,
-		TYPE_POLYGON,
-		TYPE_POINT,
-		TYPE_MAX,
-	};
+    enum MODE_TYPE {
+        TYPE_NONE,
+        TYPE_SETTING,
+        TYPE_CAMERA,
+        TYPE_VOLUME,
+        TYPE_POLYGON,
+        TYPE_POINT,
+        TYPE_MAX,
+    };
 
-    MODE_TYPE GetType() const       { return m_type;   }
-    
+    MODE_TYPE GetType() const {
+        return m_type;
+    }
 protected:
     RenderObject(MODE_TYPE t) : m_type(t) {};
-	~RenderObject(){};
-    
+    ~RenderObject() {};
 private:
-	MODE_TYPE       m_type;
-    
-protected:
+    MODE_TYPE       m_type;
 };
 
 #endif //_RENDEROBJECT_H_
