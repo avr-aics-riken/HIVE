1b6edca300f29e8b46360aa9551d71c7a42440ab
kioku <kentaro-o@digirea.com>
2015-01-27 16:37:48 +0900

    refactored camera

--- hrender/src/RenderObject/Camera.cpp
+++ hrender/src/RenderObject/Camera.cpp
@@ -4,9 +4,9 @@
 
 Camera::Camera() : RenderObject(TYPE_CAMERA)
 {
-	width  = 0;
-	height = 0;
-	outputfile = ""; //todo
+	m_width  = 0;
+	m_height = 0;
+	m_outputfile = ""; //todo
 }
 
 Camera::~Camera()
@@ -16,14 +16,14 @@ Camera::~Camera()
 
 bool Camera::SetScreenSize(unsigned int w, unsigned int h)
 {
-	width  = w;
-	height = h;
+	m_width  = w;
+	m_height = h;
 	return true;
 }
 
 bool Camera::SetFilename(const std::string& filename)
 {
-	outputfile = filename;
+	m_outputfile = filename;
 	printf("Debug: FileName = %s\n", filename.c_str());
 	return true;
 }
@@ -33,10 +33,27 @@ bool Camera::LookAt(float eye_x, float eye_y, float eye_z,
 				  float up_x,  float up_y,  float up_z,
 				  float fov)
 {
-	CameraInfo *pinfo = &info;
+	CameraInfo *pinfo = &m_info;
 	pinfo->eye[0] = eye_x; pinfo->eye[1] = eye_y; pinfo->eye[2] = eye_z;
 	pinfo->tar[0] = tar_x; pinfo->tar[1] = tar_y; pinfo->tar[2] = tar_z;
 	pinfo->up [0] = up_x;  pinfo->up [1] = up_y;  pinfo->up [2] = up_z;
 	pinfo->fov = fov;
 	return true;
 }
+
+
+int Camera::GetScreenWidth() const
+{
+    return m_width;
+}
+
+int Camera::GetScreenHeight() const
+{
+    return m_height;
+}
+
+const Camera::CameraInfo* Camera::GetCameraInfo() const
+{
+    return &m_info;
+}
+
