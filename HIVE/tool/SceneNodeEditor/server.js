2d7ce4b2875261d11d7b4bf7cd62789628c6f6be
kioku <kentaro-o@digirea.com>
2015-02-06 19:33:18 +0900

    nodeeditor list generator

--- HIVE/tool/SceneNodeEditor/server.js
+++ HIVE/tool/SceneNodeEditor/server.js
@@ -1,11 +1,57 @@
 /*jslint devel: true, browser: true*/
 /*global require, process*/
 //
-//  Simple node.js web server
+//  SceneNodeEditor
 //
 var http = require('http'),
     fs = require('fs'),
     port = 8080;
+
+//-----------------------------------------------------
+
+function makeNodeList(callback) {
+	"use strict";
+	var nodeDir = './nodes';
+	fs.readdir(nodeDir, function (err, files) {
+		var infofile,
+			nodeDirPath,
+			fileCounter,
+			customFuncLua,
+			nodelist = [],
+			i;
+		if (err) {
+			return;
+		}
+
+		fileCounter = 0;
+		function loadFunc(nodeDirPath) {
+			return function (err, data) {
+				var json = JSON.parse(data);
+				if (json.customfuncfile !== undefined) {
+					customFuncLua = fs.readFileSync(nodeDirPath + "/" + json.customfuncfile, 'utf8');
+					json.customfunc = customFuncLua;
+				}
+				nodelist.push(json);
+				fileCounter = fileCounter - 1;
+				if (fileCounter === 0) {
+					callback(null, nodelist);
+				}
+			};
+		}
+		for (i in files) {
+			if (files.hasOwnProperty(i)) {
+				nodeDirPath = nodeDir + "/" + files[i];
+				infofile = nodeDirPath + "/info.json";
+				console.log(infofile);
+				fileCounter = fileCounter + 1;
+				fs.readFile(infofile, 'utf8', loadFunc(nodeDirPath));
+			}
+		}
+	});
+}
+
+//------------------------------------------------------
+
 var server = http.createServer(function (req, res) {
     'use strict';
     console.log('REQ>', req.url);
@@ -13,6 +59,13 @@ var server = http.createServer(function (req, res) {
     if (req.url === '/') {
         file = fs.readFileSync('./root/index.html');
         res.end(file);
+	} else if (req.url === '/nodelist.json') {
+		makeNodeList((function (res) {
+			return function (err, nodelist) {
+				file = JSON.stringify(nodelist);
+				res.end(file);
+			};
+		}(res)));
     } else {
         try {
             file = fs.readFileSync('./root' + req.url.length);
@@ -28,3 +81,5 @@ if (process.argv.length > 2) {
 }
 server.listen(port);
 console.log('start server "http://localhost:' + port + '/"');
+
+
