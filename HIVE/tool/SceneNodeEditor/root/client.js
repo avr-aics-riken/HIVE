0135eb254cbf68bbf0a6c19beac092675536b9a2
masakisasaki <masaki-s@digirea.com>
2015-02-09 22:35:57 +0900

    Added component load for node ui

--- HIVE/tool/SceneNodeEditor/root/client.js
+++ HIVE/tool/SceneNodeEditor/root/client.js
@@ -1,149 +1,105 @@
 /*jslint devel:true */
-/*global SVG, svgNodeUI, io, fs */
+/*global SVG, svgNodeUI, io, fs, URL */
 
-
-var nui;
-var nodeData;
-var socket = io.connect();
-
-var staticNodeData = {
-		"nodeData":[
-			{
-				"name": "OBJLoader",
-				"pos": [150, 300],
-				"varname": "instOBJLoader",
-				"funcname": "Load",
-				"deletable": false,
-				"input": [
-					{"name": "filepath", "type": "string", "value": "'bunny.obj'"}
-				],
-				"output": [
-					{"name": "MeshData", "type": "MeshData"}
-				]
-			},
-			{
-				"name"      : "CreateCamera",
-				"pos"       : [150, 100],
-				"varname"   : "instCamera",
-				"funcname"  : "create",
-				"customfunc" : "function CreateCamera() return { camera=function(self) return self.m_cam end, create=function(self) self.m_cam = Camera(); self.m_cam:SetScreenSize(1024, 1024); self.m_cam:SetFilename('render_obj.jpg'); self.m_cam:LookAt(0, 0, -300, 0, 0, 0, 0, 1, 0, 60) end} end\n",
-				"deletable": false,
-				"cameradata" : [
-					0,0,-300,
-					0,0,0,
-					0,1,0,
-					60,
-				],
-				"input": [
-				],
-				"output": [
-					{"name": "camera", "type": "Camera"}
-				]
-			},
-			{
-				"name": "CreatePolygonModel",
-				"pos": [450, 300],
-				"varname": "instPolygonModel",
-				"funcname": "create",
-				//"customfunc" : "function CreatePolygonModel(mesh) local m_cam = PolygonModel(); m_cam:Create(mesh); m_cam:SetShader('white.frag'); return m_cam; end\n",
-				"customfunc" : "function CreatePolygonModel() return { model=function(self) return self.m_cam end, create=function(self, model) self.m_cam = PolygonModel(); self.m_cam:Create(model); self.m_cam:SetShader('white.frag'); end} end\n",
-				"deletable": false,
-				"input": [
-					{"name": "mesh", "type": "MeshData"}
-				],
-				"output": [
-					{"name": "model", "type": "RenderObject"}
-					]
-			},
-			{
-				"name": "Render",
-				"pos": [700, 100],
-				"varname": "root",
-				"funcname": "",
-				"customfunc" : "function Render() return function(...) render({...}) end end\n",
-				"deletable": false,
-				"input": [
-					{"name": "Camera",       "type": "Camera"},
-					{"name": "RenderObject", "type": "RenderObject"}
-				],
-				"output": [
-				]
-			}
-		],
-		"plugData":[
-			{"output": {"node": "instOBJLoader",    "plug": "MeshData"}, "input": {"node": "instPolygonModel", "plug": "mesh"}},
-			{"output": {"node": "instPolygonModel", "plug": "model"},     "input": {"node": "root",             "plug": "RenderObject"}},
-			{"output": {"node": "instCamera",       "plug": "camera"},     "input": {"node": "root",             "plug": "Camera"}},
-		]
-	};
-
-function reloadNode(filetext)
-{
-	//console.log(filetext);
-	var obj = JSON.parse(filetext);
-	console.log(obj);
-	nodeData = obj;
-	nui.clearNodes();
-	nui.makeNodes(nodeData);
-}
-
-function readSingleFile(e) {
-	var file = e.target.files[0];
-	if (!file) {
-		return;
-	}
-	var reader = new FileReader();
-		reader.onload = function(e) {
-		var contents = e.target.result;
-		reloadNode(contents);
-	};
-	reader.readAsText(file);
-}
-
-window.addEventListener('load', function () {
+(function () {
 	'use strict';
-
+	var nui;
+	var nodeData;
 	var socket = io.connect();
-	socket.on('connect', function () {
-		console.log('connected');
-		var sceneFile = 'print("AAA");render{}';
-		socket.emit('sendScene', JSON.stringify({scene: sceneFile}));
-		socket.on('stdio', function (data) {
-			console.log('stdio', data);
+	var nodelist;
+
+	//---------------------------------------------------------------------------
+	// request node info
+	//---------------------------------------------------------------------------
+	function loadNodeList(url) {
+		var ret;
+		var req = new XMLHttpRequest();
+		req.open('GET', url);
+		req.send();
+		req.addEventListener("load",function(ev) {
+			var resp = ev.srcElement.responseText;
+			var json = JSON.parse(resp);
+			nodelist = json;
+			console.log(resp);
+
+			//create list 
+			var d    = document.getElementById('NodeListBox');
+			if(d) {
+				var inner = '<select name="NodeList" size=' + json.length + '>';
+				for(var i = 0 ; i < json.length; i += 1) {
+					console.log(json[i]);
+					var name = json[i].name;
+					inner += '<option value="' + name + '">' + name + '</option>';
+				}
+				inner += '</select>';
+				d.innerHTML = inner;
+				d.onChange = OnSelectNodeList;
+			}
 		});
-		socket.on('stderr', function (data) {
-			console.log('stderr', data);
-		});
-	});
+	}
 
-	var	draw = SVG('nodecanvas').size(1024, 768);
-	nui = svgNodeUI(draw);
-	nodeData = staticNodeData;
-	nui.clearNodes();
-	nui.makeNodes(nodeData);
-});
-
-
-
-function ButtonEvent(id)
-{
-	console.log(id)
-	if(id === 'Clear') {
+	//---------------------------------------------------------------------------
+	// buttons
+	//---------------------------------------------------------------------------
+	function ButtonClear(e) {
+		console.log(e);
 		nui.clearNodes();
 	}
-	
-	if(id === 'Render') {
+
+	function ButtonRender(e) {
 		var customlua = nui.exportLua();
-		
-		//debug
 		console.log(customlua);
-        socket.emit('sendScene', JSON.stringify({scene: customlua}));
+		socket.emit('sendScene', JSON.stringify({scene: customlua}));
 	}
 
-	if(id === 'Open') {
-		nodeData = staticNodeData;
+	function ButtonOpen(e) {
+		nodeData = loadNodeList('nodelist.json');
+		//nui.clearNodes();
+		//nui.makeNodes(nodeData);
+	}
+
+	function ButtonAdd(e) {
+		var ele = document.getElementsByName('NodeList');
+		var index = ele[0].selectedIndex;
+		nodeData.nodeData.push(nodelist[index]);
+		console.log(nodeData);
 		nui.clearNodes();
 		nui.makeNodes(nodeData);
 	}
-}
+
+	function OnSelectNodeList(e) {
+		console.log('SELECT!!!!!!!!!!!!!!!!!!!');
+	}
+
+	//---------------------------------------------------------------------------
+	// init
+	//---------------------------------------------------------------------------
+	function init() {
+		//init canvas.
+		var draw = SVG('nodecanvas').size(1024, 768);
+		nui      = svgNodeUI(draw);
+		nui.clearNodes();
+
+		//create button
+		var openbutton   = document.getElementById('Open'),
+			renderbutton = document.getElementById('Render'),
+			clearbutton  = document.getElementById('Clear'),
+			addbutton    = document.getElementById('Add'),
+			nodelistbox  = document.getElementById('NodeList');
+
+		//handle
+		openbutton.onclick   = ButtonOpen;
+		renderbutton.onclick = ButtonRender;
+		clearbutton.onclick  = ButtonClear;
+		addbutton.onclick    = ButtonAdd;
+
+		//Initialize.
+		nodeData = {"nodeData":[], "plugData":[]};
+		console.log(nodeData);
+		loadNodeList('nodelist.json');
+	}
+
+	window.onload = init;
+
+}());
 
