0a7877091852f5a7a76f9bd8e24aaae13de4c984
kioku <kentaro-o@digirea.com>
2015-02-09 19:54:57 +0900

    add BufferSparseVolumeData

---
+++ hrender/src/Buffer/BufferSparseVolumeData.cpp
@@ -0,0 +1,57 @@
+#include "BufferSparseVolumeData.h"
+
+BufferSparseVolumeData::BufferSparseVolumeData()
+{
+    
+}
+BufferSparseVolumeData::BufferSparseVolumeData(BufferSparseVolumeData* inst)
+{
+    
+}
+BufferSparseVolumeData::~BufferSparseVolumeData()
+{
+    
+}
+void BufferSparseVolumeData::Create(int w, int h, int d, int component)
+{
+    
+}
+void BufferSparseVolumeData::AddVolume(int offset_x, int offset_y, int offset_z,
+               BufferVolumeData* vol)
+{
+    
+}
+void BufferSparseVolumeData::Clear()
+{
+    
+}
+
+void BufferSparseVolumeData::print()
+{
+    
+}
+int BufferSparseVolumeData::Width()
+{
+    
+}
+int BufferSparseVolumeData::Height()
+{
+    
+}
+int BufferSparseVolumeData::Depth()
+{
+    
+}
+int BufferSparseVolumeData::Component()
+{
+    
+}
+const std::vector<BufferSparseVolumeData::VolumeBlock>& BufferSparseVolumeData::Buffers() const
+{
+    
+}
+std::vector<BufferSparseVolumeData::VolumeBlock>& BufferSparseVolumeData::Buffers()
+{
+    
+}
+
