18a63775e6ececf4e2f247dff09362c11fe220d9
Syoyo Fujita <syoyo@lighttransport.com>
2015-02-09 19:13:24 +0900

    Update build scripts. Add more third party libs as git submodule.

--- scripts/build_loader_libs_macosx.sh
+++ scripts/build_loader_libs_macosx.sh
@@ -3,19 +3,29 @@
 topdir=`pwd`
 installdir=`pwd`/third_party/local
 
+# first clean install dir.
+rm -rf ${installdir}
+
+#
+# TextParser
+#
 cd third_party/ 
-cd TextParser
+cd TextParser/
 if [ -f "Makefile" ]; then
 	make distclean
 fi
-../configure --prefix=${installdir} && make && make install
+
+./configure --prefix=${installdir} && make && make install
 cd ${topdir}
 
+#
+# CDMlib
+#
 cd third_party/
-cd CDMlib
+cd CDMlib/
 if [ -f "Makefile" ]; then
 	make distclean
 fi
-./configure --prefix=${installdir} --with-parser=${installdir} --with-MPI=no --without-ompi && make && make install
+CXX=mpicxx CC=mpicc ./configure --prefix=${installdir} --with-parser=${installdir} --with-MPI=yes && make && make install
 cd ${topdir}
  
