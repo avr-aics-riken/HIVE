34a2762b2d841cd63ef4782729afd3b43a0cf7c1
Syoyo Fujita <syoyo@lighttransport.com>
2015-02-10 13:27:33 +0900

    Add local copy of fpzip and Zoltan package. Update third_party build script.

--- scripts/build_loader_libs_macosx.sh
+++ scripts/build_loader_libs_macosx.sh
@@ -2,30 +2,103 @@
 
 topdir=`pwd`
 installdir=`pwd`/third_party/local
+c_compiler=mpicc
+cxx_compiler=mpicxx
+cmake_bin=cmake
 
-# first clean install dir.
-rm -rf ${installdir}
+function clean_install_dir {
+	rm -rf ${installdir}
+}
 
-#
-# TextParser
-#
-cd third_party/ 
-cd TextParser/
-if [ -f "Makefile" ]; then
-	make distclean
-fi
+function build_tp {
+	#
+	# TextParser
+	#
+	cd third_party/ 
+	cd TextParser/
+	if [ -f "Makefile" ]; then
+		make distclean
+	fi
 
-./configure --prefix=${installdir} && make && make install
-cd ${topdir}
+	# TODO: MPI build of TP.
+	./configure --prefix=${installdir} && make && make install
+	cd ${topdir}
+}
 
-#
-# CDMlib
-#
-cd third_party/
-cd CDMlib/
-if [ -f "Makefile" ]; then
-	make distclean
-fi
-CXX=mpicxx CC=mpicc ./configure --prefix=${installdir} --with-parser=${installdir} --with-MPI=yes && make && make install
-cd ${topdir}
+function build_cdmlib {
+	#
+	# CDMlib
+	#
+	cd third_party/
+	cd CDMlib/
+	if [ -f "Makefile" ]; then
+		make distclean
+	fi
+	CXX=${cxx_compiler} CC=${c_compiler} ./configure --prefix=${installdir} --with-parser=${installdir} --with-MPI=yes && make && make install
+	cd ${topdir}
+}
  
+function build_bcmtools {
+	#
+	# BCMTools
+	#
+	cd third_party/
+	cd BCMTools/
+	if [ -f "Makefile" ]; then
+		make distclean
+	fi
+	CXX=${cxx_compiler} CC=${c_compiler} ./configure --prefix=${installdir} && make && make install
+	cd ${topdir}
+}
+
+function build_hdmlib {
+	#
+	# HDMlib
+	#
+	cd third_party/
+	cd HDMlib/
+	if [ -f "Makefile" ]; then
+		make distclean
+	fi
+	CXX=${cxx_compiler} CC=${c_compiler} ./configure --prefix=${installdir} --with-parser=${installdir} --with-bcm=${installdir} && make && make install
+	cd ${topdir}
+}
+
+function build_pdmlib {
+	cd third_party/
+	rm -rf fpzip-1.0.1/
+	tar -zxvf fpzip-1.0.1.tar.gz
+	cd fpzip-1.0.1/src
+	# TODO: Provide our own Makefile
+	make
+	cd ..
+	cp -Rf inc/* ${installdir}/include/
+	cp -Rf lib/* ${installdir}/lib/
+	cd ${topdir}
+
+	cd third_party/
+	rm -rf Zoltan_v3.81/
+	rm -rf Zoltan_build/
+	tar -zxvf zoltan_distrib_v3.81.tar.gz
+	mkdir Zoltan_build
+	cd Zoltan_build
+	CXX=${cxx_compiler} CC=${c_compiler} ../Zoltan_v3.81/configure --prefix=${installdir} && make && make install
+	cd ${topdir}
+
+	#
+	# PDMlib
+	#
+	cd third_party/
+	rm -rf PDMlib_build
+	mkdir PDMlib_build
+	cd PDMlib_build/
+	CXX=${cxx_compiler} CC=${c_compiler} ${cmake_bin} -DTP_ROOT=${installdir} -DFPZIP_ROOT=${installdir} -DZOLTAN_ROOT=${installdir} -DCMAKE_INSTALL_PREFIX=${installdir} ../PDMlib && make && make install
+	cd ${topdir}
+}
+
+clean_install_dir
+build_tp
+build_cdmlib
+build_bcmtools
+build_hdmlib
+build_pdmlib
