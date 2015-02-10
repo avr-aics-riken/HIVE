136fc0f83117efbcda4bf0a5ce62787089c6f514
kioku <kentaro-o@digirea.com>
2015-02-10 19:11:53 +0900

    refactored nodeeditor

--- HIVE/tool/SceneNodeEditor/nodes/CreateCamera/createcamera.lua
+++ HIVE/tool/SceneNodeEditor/nodes/CreateCamera/createcamera.lua
@@ -1,11 +1,21 @@
 function CreateCamera()
-	print('create camera')
-	local cam = Camera()
-	cam:LookAt(
-		0,0,300,
-		0,0,0,
-		0,1,0,
-		60
-	)
-	return cam;
+	local cam;
+	function create()
+		print('create camera')
+		cam = Camera()
+		cam:LookAt(
+			0,0,300,
+			0,0,0,
+			0,1,0,
+			60
+		)
+		return cam;
+	end
+	function camera()
+		return cam
+	end
+	return {
+		create = create,
+		camera = camera
+	}
 end
