030900897131c591a3d8a70604520d430b669cc7
kioku <kentaro-o@digirea.com>
2015-02-13 18:04:58 +0900

    update cmakelists

--- scripts/build_macosx.sh
+++ scripts/build_macosx.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+
+
 BUILD_PATH=`pwd`
 echo ${BUILD_PATH}
 cd ${BUILD_PATH}/../third_party/SURFACE/;
@@ -9,4 +11,6 @@ git submodule update --init
 cd ${BUILD_PATH}
 
 cmake ../
+#CXX=mpicxx CC=mpicc cmake ../ -H. -Bbuild -DHIVE_BUILD_WITH_MPI=On -DHIVE_BUILD_WITH_CDMLIB=On
+
 make
