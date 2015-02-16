dcc9f4482dbd29a41a3f01c31f8458064fa9cfd5
KazumaHatta <tori31001@gmail.com>
2015-02-16 21:56:40 +0900

    add ImageLoader/Saver (currently rgba8 tag only) add ByteBuffer

---
+++ hrender/src/Buffer/BufferImageData.h
@@ -0,0 +1,42 @@
+#ifndef _BUFFERIMAGEDATA_H_
+#define _BUFFERIMAGEDATA_H_
+
+#include "Ref.h"
+
+class FloatBuffer;
+class ByteBuffer;
+
+class BufferImageData : public RefCount
+{
+private:
+    class Impl;
+    Impl* m_imp;
+
+public:
+    enum FORMAT {
+        INVALID,
+        RGB8,
+        RGBA8,
+        RGBA32F
+    };
+    
+    BufferImageData();
+    BufferImageData(BufferImageData* inst);
+    ~BufferImageData();
+    void Create(FORMAT type, int w, int h);
+    void Clear();
+    int Width() const;
+    int Height() const;
+    FORMAT Format() const;
+    int Bytes() const;
+    FloatBuffer *FloatImageBuffer();
+    FloatBuffer *FloatImageBuffer() const;
+    ByteBuffer *ImageBuffer();
+    ByteBuffer *ImageBuffer() const;
+    float Gamma() const;
+    float Exposure() const;
+    void SetGamma(float gamma);
+    void SetExposure(float exposure);
+};
+
+#endif //_BUFFERIMAGEDATA_H_
