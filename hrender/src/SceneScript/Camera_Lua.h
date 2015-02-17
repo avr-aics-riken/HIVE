ba8be222be618b6c4e45ea09e687fcecd89236c6
KazumaHatta <tori31001@gmail.com>
2015-02-17 21:21:20 +0900

    add image functions of camera to lua

--- hrender/src/SceneScript/Camera_Lua.h
+++ hrender/src/SceneScript/Camera_Lua.h
@@ -3,12 +3,21 @@
 
 #include "LuaUtil.h"
 #include "Camera.h"
+#include "BufferImageData_Lua.h"
 
 class Camera_Lua : public Camera
 {
 private:
 	Camera_Lua(){}
 	~Camera_Lua(){}
+    
+    BufferImageData_Lua* GetImageBuffer() {
+        return new BufferImageData_Lua(Camera::GetImageBuffer());
+    }
+    
+    BufferImageData_Lua* GetDepthBuffer() {
+        return new BufferImageData_Lua(Camera::GetDepthBuffer());
+    }
     
 public:
     LUA_SCRIPTCLASS_BEGIN(Camera_Lua)
@@ -17,6 +26,8 @@ public:
     LUA_SCRIPTCLASS_METHOD_ARG1 (bool, SetNetworkAddress, const std::string&)
     LUA_SCRIPTCLASS_METHOD_ARG10(bool, LookAt, float, float, float, float, float, float, float, float, float, float)
     LUA_SCRIPTCLASS_METHOD_ARG4 (bool, ClearColor, float, float, float, float)
+    LUA_SCRIPTCLASS_METHOD_ARG0 (BufferImageData_Lua*,GetImageBuffer)
+    LUA_SCRIPTCLASS_METHOD_ARG0 (BufferImageData_Lua*,GetDepthBuffer)
     LUA_SCRIPTCLASS_END();
 };
 LUA_SCRIPTCLASS_CAST_AND_PUSH(Camera_Lua);
