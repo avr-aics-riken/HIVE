acf155ac48636513cd5fdebcff71082f9a458ff9
Masaki <masakisasaki@gmail.com>
2015-02-20 19:48:18 +0900

    Create TetraData WIP

---
+++ hrender/src/Buffer/BufferTetraData.h
@@ -0,0 +1,32 @@
+#ifndef _BUFFERTETRADATA_H_
+#define _BUFFERTETRADATA_H_
+
+#include "Ref.h"
+
+class Vec3Buffer;
+class Vec2Buffer;
+class FloatBuffer;
+class UintBuffer;
+
+class BufferTetraData : public RefCount
+{
+private:
+    class Impl;
+    Impl* m_imp;
+    
+public:
+    BufferTetraData(BufferTetraData* inst);
+    BufferTetraData();
+    ~BufferTetraData();
+    void Create(int vertexnum, int indexnum);
+    void Clear();
+    void print();
+    Vec3Buffer*  Position() ;
+    UintBuffer*  Index()    ;
+    Vec3Buffer*  Position() const;
+    UintBuffer*  Index()    const;
+
+};
+
+#endif //_BUFFERTETRADATA_H_
+
---
+++ hrender/src/Builder/VolumeToTetraData.cpp
@@ -0,0 +1,68 @@
+#include "VolumeToTetraData.h"
+
+VolumeToTetraData::VolumeToTetraData(){
+    m_tetra = new BufferTetraData();
+}
+
+
+inline void GenerateTetra(
+  float* out_vertices,
+  float* in_vertices, double bmin[3], double bmax[3], double scale)
+{
+  double u0 = in_vertices[0];
+  double u1 = in_vertices[1];
+  double u2 = in_vertices[2];
+
+  //TODO const
+  double offset[4][3];
+  offset[0][0] = 1.0;
+  offset[0][1] = 0.0;
+  offset[0][2] = 0.0;
+  offset[1][0] = 1.0;
+  offset[1][1] = 0.0;
+  offset[1][2] = 1.0;
+  offset[2][0] = 0.0;
+  offset[2][1] = 0.0;
+  offset[2][2] = 1.0;
+  offset[3][0] = 0.0;
+  offset[3][1] = 1.0;
+  offset[3][2] = 0.0;
+
+  double px = (bmax[0] - bmin[0]) * u0 + bmin[0];
+  double py = (bmax[1] - bmin[1]) * u1 + bmin[1];
+  double pz = (bmax[2] - bmin[2]) * u2 + bmin[2];
+  for (size_t j = 0; j < 4; j++) {
+    double v0 = scale * (bmax[0] - bmin[0]) * offset[j][0];
+    double v1 = scale * (bmax[1] - bmin[1]) * offset[j][1];
+    double v2 = scale * (bmax[2] - bmin[2]) * offset[j][2];
+    out_vertices[3 * (4 * j) + 0] = px + v0;
+    out_vertices[3 * (4 * j) + 1] = py + v1;
+    out_vertices[3 * (4 * j) + 2] = pz + v2;
+  }
+}
+
+int VolumeToTetraData::Create(BufferVolumeData *volume, int divide)
+    if (!volume)
+        return 0;
+    
+    //todo custom
+    double bmin[3] = {-1, -1, -1};
+    double bmax[3] = { 1,  1,  1};
+    double scale   = 0.04;
+    const int volume_num = volume->Buffer()->GetNum();
+    int tetra_vertex_num = volume_num * 4;
+    float *source        = volume->Buffer()->GetBuffer();
+    m_tetra->Buffer()->Create(tetra_vertex_num);
+    float *dest          = m_tetra->Buffer()->GetBuffer();
+    for (int i = 0; i < volume_num; ++i){
+        GenerateTetra(&dest[i * 4], &source[i * 3], bmin, bmax, scale);
+    }
+    return m_tetra->Buffer()->GetNum();
+}
+
+
+BufferVolumeData* VolumeToTetraData::VolumeData()
+{
+    return m_tetra;
+}
+
