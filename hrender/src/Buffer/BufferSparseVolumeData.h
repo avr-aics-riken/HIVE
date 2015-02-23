12e37835c1bb8a89ec685edf74bd4f3be1cc3ad5
Syoyo Fujita <syoyo@lighttransport.com>
2015-02-23 18:05:18 +0900

    Add Sample() feature to look up voxel value for Volume data. Add isosurface generator.

--- hrender/src/Buffer/BufferSparseVolumeData.h
+++ hrender/src/Buffer/BufferSparseVolumeData.h
@@ -32,6 +32,15 @@ public:
     int Component();
     const std::vector<VolumeBlock>& Buffers() const;
     std::vector<VolumeBlock>& Buffers();
+
+    // Fetch voxel data. `ret` pointer must have enough storage space to store voxel data,
+    // larger than # of compoents in this volume data.
+    // Assume x, y and z are in the range of unit space: i,e. [0, 1)^3
+    void Sample(float* ret, float x, float y, float z);
+
+    // Build spatial acceraltion for SparseVolume.
+    // Only required if you want to fetch voxel data through Sample() function.
+    bool Build();
 };
 
 #endif //_BUFFERSPARSEVOLUMEDATA_H_
