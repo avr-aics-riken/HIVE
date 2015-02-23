0497d43a0f578d79252a33ff5279317e3f862291
kioku <kentaro-o@digirea.com>
2015-02-23 16:00:40 +0900

    fixed RenderObject model interface

--- hrender/src/SceneScript/LineModel_Lua.h
+++ hrender/src/SceneScript/LineModel_Lua.h
@@ -24,6 +24,16 @@ public:
     LUA_SCRIPTCLASS_METHOD_ARG1(bool, SetShader, const std::string&)
     LUA_SCRIPTCLASS_METHOD_ARG1(bool, SetLineWidth, float)
     LUA_SCRIPTCLASS_METHOD_ARG1(bool, Create, BufferLineData_Lua*)
+
+    // RenderObject interface
+    LUA_SCRIPTCLASS_METHOD_ARG3(bool, SetTranslate,float, float, float)
+    LUA_SCRIPTCLASS_METHOD_ARG3(bool, SetRotate,float, float, float)
+    LUA_SCRIPTCLASS_METHOD_ARG3(bool, SetScale,float, float, float)
+    LUA_SCRIPTCLASS_METHOD_ARG3(bool, SetVec2,  const char*, float, float)
+    LUA_SCRIPTCLASS_METHOD_ARG4(bool, SetVec3,  const char*, float, float, float)
+    LUA_SCRIPTCLASS_METHOD_ARG5(bool, SetVec4,  const char*, float, float, float, float)
+    LUA_SCRIPTCLASS_METHOD_ARG2(bool, SetFloat, const char*, float)
+
     LUA_SCRIPTCLASS_END();
 };
 LUA_SCRIPTCLASS_CAST_AND_PUSH(LineModel_Lua);
