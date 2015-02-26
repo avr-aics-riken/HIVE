c072e5c59f5e46a9f64bc4be78bd0250b9a1bbee
Syoyo Fujita <syoyo@lighttransport.com>
2015-02-27 00:52:30 +0900

    Use static link in build script. Add `hrender` for install target.

--- scripts/cmake_linux-x64.sh
+++ scripts/cmake_linux-x64.sh
@@ -2,4 +2,5 @@
 
 # Assume mpicc has been installed
 
-CXX=mpicxx CC=mpicc cmake28 -H. -Bbuild -DHIVE_BUILD_WITH_MPI=On -DHIVE_BUILD_WITH_CDMLIB=On
+#CXX=mpicxx CC=mpicc cmake28 -H. -Bbuild -DHIVE_BUILD_WITH_MPI=On -DHIVE_BUILD_WITH_CDMLIB=On -DHIVE_BUILD_WITH_HDMLIB=On -DHIVE_BUILD_WITH_PDMLIB=On -DBUILD_SHARED_LIBS=Off
+cmake28 -H. -Bbuild -DBUILD_SHARED_LIBS=Off
