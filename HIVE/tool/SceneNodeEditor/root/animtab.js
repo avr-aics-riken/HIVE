8a4ed911fc8995033d523e9af8a1fa33619ed02b
KazumaHatta <tori31001@gmail.com>
2015-02-27 15:38:28 +0900

    update animation tab

--- HIVE/tool/SceneNodeEditor/root/animtab.js
+++ HIVE/tool/SceneNodeEditor/root/animtab.js
@@ -1,10 +1,11 @@
 /*jslint devel:true */
-/*global $, $ready, $animate, io, fs, URL, FileReader */
+/*global $, $ready, $animate, io, fs, URL */
 (function () {
 	"use strict";
 
 	var animtab = {},
-		buttonNum = 0;
+		buttonNum = 0,
+		initialOverflow = {};
 	
 	function to_json(key, value) {
 		var json = {};
@@ -12,8 +13,95 @@
 		return json;
 	}
 	
+	function to_num(pixelStr) {
+		return pixelStr.split('px').join('');
+	}
+	
+	/// initialize dialog and set separator 
+	function setupSeparator(direction, separator, button, targets, whstr) {
+		var dragging = false,
+			buttonID = Object.keys(button)[0],
+			targetID = Object.keys(targets)[0],
+			buttonElem = document.getElementById(buttonID),
+			diffButtonTargetMax = 0;
+		
+		diffButtonTargetMax = to_num(button[buttonID].max) - to_num(targets[targetID].max);
+		console.log("diffButtonTargetMax" + diffButtonTargetMax);
+		
+		separator.onmousedown = function (e) {
+			var target,
+				id;
+			dragging = true;
+			for (id in targets) {
+				if (targets.hasOwnProperty(id)) {
+					target = document.getElementById(id);
+					target.style.overflow = "hidden";
+				}
+			}
+		};
+		separator.onmouseover = function (e) {
+			if (direction === 'left') {
+				separator.style.cursor = "w-resize";
+			} else if (direction === 'right') {
+				separator.style.cursor = "e-resize";
+			} else if (direction === 'top') {
+				separator.style.cursor = "s-resize";
+			} else if (direction === 'bottom') {
+				separator.style.cursor = "n-resize";
+			}
+		};
+		document.addEventListener('mouseup', function (e) {
+			var target,
+				id;
+			dragging = false;
+			for (id in targets) {
+				if (targets.hasOwnProperty(id)) {
+					target = document.getElementById(id);
+					target.style.overflow = initialOverflow[id];
+				}
+			}
+		});
+		document.addEventListener('mousemove', function (e) {
+			var offset,
+				pos,
+				i,
+				target,
+				id;
+			if (dragging) {
+				if (direction === 'left') {
+					offset = window.pageXOffset || document.documentElement.scrollLeft;
+					pos = offset + e.clientX;
+				} else if (direction === 'right') {
+					offset = window.pageXOffset || document.documentElement.scrollLeft;
+					pos = document.documentElement.clientWidth - (offset + e.clientX);
+				} else if (direction === 'top') {
+					offset = window.pageYOffset || document.documentElement.scrollTop;
+					pos = offset + e.clientY;
+				} else if (direction === 'bottom') {
+					offset = window.pageYOffset || document.documentElement.scrollTop;
+					pos = document.documentElement.clientHeight - (offset + e.clientY);
+				}
+				if (pos < 5) {
+					pos = 0;
+				}
+				//console.log(direction + ":" + pos);
+				for (id in targets) {
+					if (targets.hasOwnProperty(id)) {
+						target = document.getElementById(id);
+						target.style[whstr] = pos + 'px';
+						targets[id].max = pos + 'px';
+					}
+				}
+				separator.style[direction] = pos + 'px';
+				buttonElem.style[direction] = (pos + diffButtonTargetMax) + 'px';
+				button[buttonID].max = (pos + diffButtonTargetMax) + 'px';
+			}
+		});
+	}
+	
 	function create(direction, button, targets, beforeLabel, afterLabel) {
 		var buttonElem = document.createElement("input"),
+			separatorElem = document.createElement("span"),
 			buttonID = Object.keys(button)[0],
 			buttonMin = button[buttonID].min,
 			buttonMax = button[buttonID].max,
@@ -21,50 +109,103 @@
 			json,
 			time = 1000,
 			state = 0,
-			initialTargetMin = {},
-			initialTargetMax = {};
+			targetElem,
+			targetMin,
+			targetMax,
+			minimum = Number.MAX_VALUE,
+			maximum = Number.MIN_VALUE,
+			temp,
+			id;
 		
 		buttonElem.type = "button";
 		buttonElem.id = buttonID;
 		buttonElem.className = buttonID;
 		buttonElem.style.position = "absolute";
 		buttonElem.value = beforeLabel;
-		document.body.appendChild(buttonElem);
 		
 		if (direction === 'bottom' || direction === 'top') {
 			whstr = 'height';
+			separatorElem.style.width = "100px";
+			separatorElem.style.height = "20px";
 		} else if (direction === 'right' || direction === 'left') {
 			whstr = 'width';
+			separatorElem.style.width = "20px";
+			separatorElem.style.height = "100px";
 		}
+		separatorElem.id = buttonID + "_separator";
+		separatorElem.className = buttonID + "_separator";
+		separatorElem.style.background = "rgba(100, 100, 100, 0.5)";
+		separatorElem.style.position = "absolute";
+		separatorElem.style.border = "solid";
+		separatorElem.style.borderRadius = "6px";
+		separatorElem.style.MozBorderRadius = "6px";
+		separatorElem.style.WebkitBorderRadius = "6px";
 		
+		// add elements
+		document.body.appendChild(separatorElem);
+		document.body.appendChild(buttonElem);
+		
+		for (id in targets) {
+			if (targets.hasOwnProperty(id)) {
+				targetElem = $(id);
+				initialOverflow[id] = targetElem.style.overflow;
+				targetMin = targets[id].min;
+				targetMax = targets[id].max;
+				temp = to_num(targetMin);
+				if (temp < minimum) {
+					minimum = temp;
+				}
+				temp = to_num(targetMax);
+				console.log(temp);
+				if (temp > maximum) {
+					maximum = temp;
+				}
+			}
+		}
+		console.log("maximum:" + maximum);
+		separatorElem.style[direction] = maximum + 'px';
+		if (buttonMin === 'auto') {
+			buttonMin = minimum + 'px';
+		}
+		if (buttonMax === 'auto') {
+			buttonMax = maximum + 'px';
+		}
+		console.log("buttonMin:" + buttonMin);
+		console.log("buttonMax:" + buttonMax);
+		button[buttonID].min = buttonMin;
+		button[buttonID].max = buttonMax;
+		
+		setupSeparator(direction, separatorElem, button, targets, whstr);
+			
 		function createAnimateButton() {
 			var i = 0,
 				id,
 				targetElem,
 				targetMin,
-				targetMax,
-				temp,
-				minimum = Number.MAX_VALUE,
-				maximum = Number.MIN_VALUE;
+				targetMax;
 			
+			buttonMin = button[buttonID].min;
+			buttonMax = button[buttonID].max;
+
 			function beforeTarget() {
 				state = 2;
 			}
 			
 			function afterTarget() {
-				targetElem.style.overflow = "auto";
+				targetElem.style.overflow = initialOverflow[id];
 				state = 0;
 			}
 			
 			function beforeButton() {
 				buttonElem.value = afterLabel;
-				state = 2;
 			}
 			
 			function afterButton() {
 				buttonElem.value = beforeLabel;
-				state = 0;
 			}
+			
+			function beforeSep() {}
+			function afterSep() {}
 			
 			for (id in targets) {
 				if (targets.hasOwnProperty(id)) {
@@ -72,45 +213,18 @@
 					targetElem.style.overflow = "hidden";
 					targetMin = targets[id].min;
 					targetMax = targets[id].max;
-					/*
-					if (targetMin === 'auto') {
-						if (state === 0) {
-							initialTargetMin[id] = targetElem.style[direction];
-						}
-						targetMin = initialTargetMin[id];
-					}
-					if (targetMax === 'auto') {
-						if (state === 0) {
-							initialTargetMax[id] = targetElem.style[whstr];
-						}
-						targetMax = initialTargetMax[id];
-					}
-					*/
-					temp = targetMin.split('px').join('');
-					if (temp < minimum) {
-						minimum = temp;
-					}
-					temp = targetMax.split('px').join('');
-					if (temp > maximum) {
-						maximum = temp;
-					}
 					if (state === 0) {
 						state = 1;
 						$animate(targetElem, to_json(whstr, { from: targetMax, to: targetMin }), time, beforeTarget);
 						$animate(buttonElem, to_json(direction, { from : buttonMax, to : buttonMin }), time, beforeButton);
+						$animate(separatorElem, to_json(direction, { from : buttonMax, to : buttonMin }), time, beforeSep);
 					} else if (state === 2) {
 						state = 3;
 						$animate(targetElem, to_json(whstr, { from: targetMin, to: targetMax }), time, afterTarget);
 						$animate(buttonElem, to_json(direction, { from : buttonMin, to : buttonMax }), time, afterButton);
+						$animate(separatorElem, to_json(direction, { from : buttonMin, to : buttonMax }), time, afterSep);
 					}
 				}
-			}
-			
-			if (buttonMin === 'auto') {
-				buttonMin = minimum + 'px';
-			}
-			if (buttonMax === 'auto') {
-				buttonMax = maximum + 'px';
 			}
 		}
 		
@@ -126,5 +240,3 @@
 	window.animtab = animtab;
 	window.animtab.create = create;
 }());
-
-
