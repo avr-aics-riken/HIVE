a50958d5006571b1dadaf24d0bb7cf1f86be8c0e
KazumaHatta <tori31001@gmail.com>
2015-03-03 18:53:18 +0900

    add metabinary for SIP

---
+++ hrender/src/Network/MetaBinary.h
@@ -0,0 +1,25 @@
+#ifndef _METABINARY_H_
+#define _METABINARY_H_
+
+#include "../Core/Ref.h"
+
+class MetaBinary : public RefCount
+{
+private:
+    class Impl;
+    Impl* m_imp;
+    
+public:
+    typedef void* Buffer;
+    
+    MetaBinary();
+    ~MetaBinary();
+    
+    bool Create(const std::string& json, const Buffer binary, int binarySize);
+    
+    const Buffer BinaryBuffer() const;
+
+    int BinaryBufferSize() const;
+};
+
+#endif //_METABINARY_H_
