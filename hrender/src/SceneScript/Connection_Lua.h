8d2ce183c69a7ffddcd6c1fd0bbf08ecf95cb495
kioku <kentaro-o@digirea.com>
2015-03-06 23:53:08 +0900

    update for HIVE app

--- hrender/src/SceneScript/Connection_Lua.h
+++ hrender/src/SceneScript/Connection_Lua.h
@@ -18,6 +18,7 @@ public:
     LUA_SCRIPTCLASS_METHOD_ARG2 (bool, SendBinary, const Connection::Binary, int)
     LUA_SCRIPTCLASS_METHOD_ARG1 (bool, SendImage, const std::string&)
     LUA_SCRIPTCLASS_METHOD_ARG0 (std::string, Recv)
+    LUA_SCRIPTCLASS_METHOD_ARG0 (std::string, GetState)
     LUA_SCRIPTCLASS_METHOD_ARG1 (bool, SetTimeout, int)
     LUA_SCRIPTCLASS_METHOD_ARG0 (bool, Close)
     LUA_SCRIPTCLASS_END();
