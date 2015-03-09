0f9bdf48559add832d571b9375e4714fc40f7211
Syoyo Fujita <syoyo@lighttransport.com>
2015-03-09 17:27:31 +0900

    Force turn SURFACE_BUILD_WITH_OPENMP flag on when HIVE_BUILD_WITH_OPENMP was set to on.

--- scripts/cmake_macosx_omp.sh
+++ scripts/cmake_macosx_omp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-CC=/usr/local/bin/gcc CXX=/usr/local/bin/g++ cmake .. -DSURFACE_BUILD_WITH_OPENMP=On
+CC=/usr/local/bin/gcc CXX=/usr/local/bin/g++ cmake .. -DHIVE_BUILD_WITH_OPENMP=On
 
