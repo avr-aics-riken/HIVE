d474fadc0ed00ba058fba04ae69d9d4fd7630067
kioku <kentaro-o@digirea.com>
2015-03-14 13:11:31 +0900

    upate HIVE

--- HIVE/HIVE/root/js/HiveCore.js
+++ HIVE/HIVE/root/js/HiveCore.js
@@ -1,7 +1,8 @@
 /*jslint devel:true*/
 /*global Blob, URL*/
 /*global HiveConnect*/  // HiveConnect.js
-/*global HiveCommand*/ // HiveCommand.js
+/*global HiveCommand*/  // HiveCommand.js
+/*global vec3, vec4, mat4, subtract, add, scale, rotate, cross, translate, dot, normalize, transpose, mat3 */ // MV.js
 
 (function (window) {
 	'use strict';
@@ -13,6 +14,7 @@
 		return function (err, res) {
 			if (err) {
 				console.error('runscript error:', err, {script: src});
+				console.trace();
 			} else {
 				console.log('runscript result:', res, {script: src});
 			}
@@ -21,25 +23,31 @@
 	function runScript(conn, src, callback) {
 		conn.rendererMethod('runscript', {script: src}, callback || defaultCallback(src));
 	}
-	function registerMethods(hiveCore, resultElement) {
+	function registerMethods(hiveCore, resultElement, infoCallback) {
 		if (!hiveCore.conn) { return; }
 		
 		hiveCore.conn.method('open', function (res) {
-			hiveCore.render();
+			hiveCore.updateSceneInformation();
 		});
 
+		hiveCore.conn.method('updateInfo', infoCallback);/*function (param, data) {
+			console.log('AAAAAAAA:updateInfo', param, data);
+		});*/
+		
 		hiveCore.conn.method('renderedImage', function (param, data) {
 			if (param.type === 'jpg') {
 				resultElement.src = URL.createObjectURL(new Blob([data], {type: "image/jpeg"}));
 			}
 			
 			// Refine render
-			/*if (!param.canceled) {
-				if (param.width < camera_maxsize[0] || param.height < camera_maxsize[1]) {
+			/*
+			if (!param.canceled) {
+				if (param.width <= hiveCore.screenSize[0] && param.height <= hiveCore.screenSize[1]) {
 					//console.log('REFINE RENDER!', param.width * 2, param.height * 2);
-					cmd = sceneCommands.cameraScreenSize('camera', param.width * 2, param.height * 2);
-					cmd += sceneCommands.render('view');
-					runScript(cmd);
+					var w = Math.min(param.width * 2, hiveCore.screenSize[0] * 2),
+						h = Math.min(param.height * 2, hiveCore.screenSize[1] * 2),
+						cmd = HiveCommand.renderCamera(w, h, 'view');
+					runScript(hiveCore.conn, cmd);
 				}
 			}*/
 		});
@@ -48,11 +56,14 @@
 	//----------------------------------------------------------------------------------------------
 	// Contructor
 	//
-	var HiveCore = function (resultElement, width, height) {
+	var HiveCore = function (resultElement, width, height, infoCallback) {
 		this.conn = new HiveConnect();
 		this.resize(width, height);
 		this.modelCount = 0;
-		registerMethods(this, resultElement);
+		this.viewCamera = {pos: vec3(0, 0, 300), tar: vec3(0, 0, 0), up: vec3(0, 1, 0), fov: 60};
+		this.objectList = [];
+		this.updateSceneCallback = infoCallback;
+		registerMethods(this, resultElement, infoCallback);
 	};
 
 	//----------------------------------------------------------------------------------------------
@@ -66,8 +77,9 @@
 		// TODO: Temp camera
 		var cmd = '';//sceneCommands.createCamera('camera');
 		//cmd += sceneCommands.cameraScreenSize('camera', this.screenSize[0], this.screenSize[1]);
-		cmd += HiveCommand.cameraPos('view', [0, 0, 300]);
-		cmd += HiveCommand.renderCamera(this.screenSize[0], this.screenSize[1], 'view');
+		//cmd += HiveCommand.cameraPos('view', [0, 0, 300]);
+		cmd += HiveCommand.cameraLookat('view', this.viewCamera.pos, this.viewCamera.tar, this.viewCamera.up, this.viewCamera.fov);
+		cmd += HiveCommand.renderCamera(parseInt(this.screenSize[0] / 8, 10), parseInt(this.screenSize[1] / 8, 10), 'view');
 		runScript(this.conn, cmd);
 	};
 	
@@ -95,12 +107,11 @@
 	//----------------------------------------------------------------------------------------------
 	// Information
 	//
-	HiveCore.prototype.getSceneInformation = function () {
-		var cmd = HiveCommand.getObjectList();
-		runScript(this.conn, cmd, function (err, res) {
-			console.log(err, res);
-		});
+	HiveCore.prototype.updateSceneInformation = function () {
+		var cmd = HiveCommand.updateSceneInformation();
+		runScript(this.conn, cmd);
 	};
+	
 	//----------------------------------------------------------------------------------------------
 	// Scene operation
 	//
@@ -110,7 +121,64 @@
 		runScript(this.conn, cmd);
 		this.render();
 	};
-
+	
+	HiveCore.prototype.addCamera = function (name) {
+		var cmd = '';
+		if (name) {
+			cmd = HiveCommand.createCamera(name);
+		} else {
+			cmd = HiveCommand.createCamera('Camera-' + this.modelCount);
+		}
+		this.modelCount = this.modelCount + 1;
+		runScript(this.conn, cmd);
+	};
+	
+	HiveCore.prototype.deleteObject = function (name) {
+		// delete
+		//TODO:
+		runScript(this.conn, HiveCommand.deleteObject(name));
+	};
+	
+	//----------------------------------------------------------------------------------------------
+	// View operation
+	//
+	HiveCore.prototype.Rotate = function (rotx, roty) {
+		var eyedir = subtract(this.viewCamera.pos, this.viewCamera.tar),
+			v = vec4(eyedir[0], eyedir[1], eyedir[2], 0.0),
+			rx = rotate(roty, vec3(0, 1, 0)),
+			ry = rotate(rotx, vec3(1, 0, 0));
+		v = vec4(dot(ry[0], v), dot(ry[1], v), dot(ry[2], v), 0.0);
+		v = vec3(dot(rx[0], v), dot(rx[1], v), dot(rx[2], v));
+		this.viewCamera.pos = add(this.viewCamera.tar, v);
+		this.render();
+	};
+	HiveCore.prototype.Zoom = function (zoom) {
+		var v = subtract(this.viewCamera.pos, this.viewCamera.tar),
+			r = 1.0 - (zoom / 1000.0);
+		v = scale(r, v);
+		console.log(v);
+		this.viewCamera.pos = add(this.viewCamera.tar, v);
+		this.render();
+	};
+	HiveCore.prototype.Translate = function (tx, ty) {
+		/*
+			TODO: FIX ME
+		*/
+		var mv = vec3(-tx, ty, 0.0),
+			az = normalize(subtract(this.viewCamera.pos, this.viewCamera.tar)),
+			ax = normalize(cross(vec3(0, 1, 0), az)),
+			ay = normalize(cross(az, ax)),
+			mx = transpose(mat3(ax, ay, az)),
+			mm = vec3(vec3(dot(mv, mx[0]), dot(mv, mx[1]), dot(mv, mx[2])));
+		this.viewCamera.pos = add(this.viewCamera.pos, mm);
+		this.viewCamera.tar = add(this.viewCamera.tar, mm);
+		this.render();
+	};
+	
+	//----------------------------------------------------------------------------------------------
+	// Callback
+	//
+	
 	//----------------------------------------------------------------------------------------------
 	
 	HiveCore.prototype.updateTime = function (tm) {
--- HIVE/HIVE/root/js/client.js
+++ HIVE/HIVE/root/js/client.js
@@ -2,14 +2,54 @@
 /*global HiveCore*/            // HiveCore
 /*global kvtoolsUI_init, kUI*/ // kvtoolsUI
 /*global $, $toggle*/          // qatrix
+/*global */
 
 (function (window) {
 	'use strict';
+	
+	function updateProperty(objname) {
+		
+	}
+	
+	function objectClick(core, objname) {
+		return function () {
+			updateProperty(objname);
+		};
+	}
+	function dustClick(core, objname) {
+		return function () {
+			console.log('dust:', objname);
+			core.deleteObject(objname);
+		};
+	}
+	
 	function init() {
 		kvtoolsUI_init();
-		var core = new HiveCore($('result'),
-								document.getElementById('window-view').clientWidth,
-								document.getElementById('window-view').clientHeight);
+		var mouseState = {"Left": false, "Center": false, "Right": false, "x": 0, "y": 0 },
+			core;
+		
+		function updateObjList(sceneInfo) {
+			console.log(sceneInfo);
+			var i,
+				objlist = sceneInfo.objectlist,
+				lst = kUI('list-itemlist'),
+				dust,
+				itemid;
+			lst.Clear();
+			for (i = 0; i < objlist.length; i = i + 1) {
+				//objList[i].name, objList[i].type
+				itemid = 'listitem-' + objlist[i].name;
+				lst.AddItem(objlist[i].name, itemid);
+				$(itemid).addEventListener('click', objectClick(core, objlist[i].name));
+				$(itemid + '-dustbtn').addEventListener('click', dustClick(core, objlist[i].name));
+			}
+			core.render();
+		}
+		
+		core = new HiveCore($('result'),
+							document.getElementById('window-view').clientWidth,
+							document.getElementById('window-view').clientHeight,
+							updateObjList);
 	
 		// Initial Resize
 		// Resize Event
@@ -30,7 +70,8 @@
 		// disable context menu
 		window.addEventListener('contextmenu', function (e) {
 			//console.log('context');
-			core.loadOBJ('bunny.obj', 'normal.frag');
+			//core.loadOBJ('bunny.obj', 'normal.frag');
+			//core.render();
 			e.preventDefault();
 		});
 
@@ -41,7 +82,7 @@
 		kUI('timeline').setTimelineData();
 		kUI('timeline').drawGraph();
 		kUI('timeline').ChangeTimeCallback(function (tm) {
-			kUI('viewmode').SetCaption('[Camera]');
+			//kUI('viewmode').SetCaption('[Camera]');
 			core.updateTime(tm);
 		});
 		$('projsetting').addEventListener('click', function (ev) {
@@ -50,20 +91,63 @@
 		$('showconsole').addEventListener('click', function (ev) {
 			$toggle($('consoleArea'), 500);
 		});
-		
-		$('resultdiv').addEventListener('mousedown', function (ev) {
-			ev.preventDefault();
-		});
-		$('resultdiv').addEventListener('mouseup', function (ev) {
-			ev.preventDefault();
-		});
-		$('resultdiv').addEventListener('mousemove', function (ev) {
-			ev.preventDefault();
-		});
 		$('newscenebtn').addEventListener('click', function (ev) {
 			ev.preventDefault();
 			core.newScene();
 		});
+		$('loadscenebtn').addEventListener('click', function (ev) {
+			core.loadOBJ('bunny.obj', 'normal.frag');
+			core.render();
+		});
+		$('list-cameraaddbutton').addEventListener('click', function (ev) {
+			core.addCamera();
+		});
+
+		//$('savescenebtn').addEventListener('click', function (ev) {
+			//core.getSceneInformation(function (objList) {
+			//);
+		//});
+
+		// ----------------------------
+		// Mouse Event
+		function mouseDown(e) {
+			e.preventDefault();
+			if (e.button === 0) { mouseState.Left   = true; }
+			if (e.button === 2) { mouseState.Right  = true; }
+			if (e.button === 1) { mouseState.Center = true; }
+			mouseState.x = e.clientX;
+			mouseState.y = e.clientY;
+		}
+
+		function mouseUp(e) {
+			e.preventDefault();
+			if (e.button === 0) { mouseState.Left   = false; }
+			if (e.button === 2) { mouseState.Right  = false; }
+			if (e.button === 1) { mouseState.Center = false; }
+			mouseState.x = e.clientX;
+			mouseState.y = e.clientY;
+		}
+
+		function mouseMove(e) {
+			e.preventDefault();
+			var dx = e.clientX - mouseState.x,
+				dy = e.clientY - mouseState.y;
+
+			if (mouseState.Left) {
+				core.Rotate(dy * -0.5, dx * -0.5); // Swizzle axis
+			}
+			if (mouseState.Right) {
+				core.Zoom(dx + dy);
+			}
+			if (mouseState.Center) {
+				core.Translate(dx, dy);
+			}
+			mouseState.x = e.clientX;
+			mouseState.y = e.clientY;
+		}
+		$('resultdiv').addEventListener('mousedown', mouseDown);
+		$('resultdiv').addEventListener('mouseup',   mouseUp);
+		$('resultdiv').addEventListener('mousemove', mouseMove);
 		//----------------------------
 	}
 	window.addEventListener('load', init);
