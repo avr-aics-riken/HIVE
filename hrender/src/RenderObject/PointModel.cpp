d259b2a7e680f82bb19318c2f09236e6281bfc9f
KazumaHatta <tori31001@gmail.com>
2015-03-18 16:27:08 +0900

    add doxygen comment

--- hrender/src/RenderObject/PointModel.cpp
+++ hrender/src/RenderObject/PointModel.cpp
@@ -1,3 +1,7 @@
+/**
+ * @file PointModel.cpp
+ * ポイントモデル
+ */
 #include <string>
 #include "PointModel.h"
 #include "BufferPointData.h"
@@ -5,21 +9,33 @@
 class PointModel::Impl
 {
 public:
+    /// コンストラクタ
     Impl()
     {
         m_point = 0;
     }
+    
+    /// デストラクタ
     ~Impl()
     {
         
     }
 
+    /**
+     * シェーダパスの設定
+     * @param shaderfile シェーダパス
+     */
     bool SetShader(const std::string& shaderfile)
     {
         m_shaderfile = shaderfile;
         return true;
     }
     
+    /**
+     * ポイントデータの生成
+     * @retval true 成功 
+     * @retval false 失敗
+     */
     bool Create(BufferPointData* pointdata)
     {
         if (!pointdata)
@@ -28,10 +44,18 @@ public:
         return true;
     }
     
+    /**
+     * ポイントデータの取得.
+     * @retval ポイントデータ
+     */
     BufferPointData* GetPoint() const {
         return m_point;
     }
     
+    /**
+     * シェーダパスを返す.
+     * @retval シェーダパス
+     */
     const std::string& GetShader() const
     {
         return m_shaderfile;
@@ -42,31 +66,51 @@ private:
     std::string m_shaderfile;
 
 };
+
+/// コンストラクタ
 PointModel::PointModel() : RenderObject(TYPE_POINT)
 {
     m_imp = new Impl();
 }
 
+/// デストラクタ
 PointModel::~PointModel()
 {
     delete m_imp;
 }
 
+/**
+ * シェーダパスの設定
+ * @param shaderfile シェーダパス
+ */
 bool PointModel::SetShader(const std::string& shaderfile)
 {
     return m_imp->SetShader(shaderfile);
 }
 
+/**
+ * ポイントデータの生成
+ * @retval true 成功
+ * @retval false 失敗
+ */
 bool PointModel::Create(BufferPointData* pointdata)
 {
     return m_imp->Create(pointdata);
 }
 
+/**
+ * ポイントデータの取得.
+ * @retval ポイントデータ
+ */
 BufferPointData* PointModel::GetPoint() const
 {
     return m_imp->GetPoint();
 }
 
+/**
+ * シェーダパスを返す.
+ * @retval シェーダパス
+ */
 const std::string& PointModel::GetShader() const
 {
     return m_imp->GetShader();
--- hrender/src/RenderObject/RenderObject.cpp
+++ hrender/src/RenderObject/RenderObject.cpp
@@ -1,13 +1,19 @@
+/**
+ * @file RenderObject.cpp
+ * レンダーオブジェクト
+ */
 #include "RenderObject.h"
 #include "../Buffer/BufferImageData.h"
 
 class RenderObject::Impl
 {
 public:
+    /// モードタイプを返す.
     RenderObject::MODE_TYPE GetType() const {
         return m_type;
     }
 
+    /// タイプ文字列を返す.
     std::string GetTypeString() const {
         static const char* modeString[] = {
             "NONE",
@@ -26,9 +32,13 @@ public:
     //--------------------------------------------------
     //Get
     //--------------------------------------------------
+    /// 移動量を返す
     const VX::Math::vec3&  GetTranslate() const { return m_trans; }
+    /// 回転量を返す
     const VX::Math::vec3&  GetRotate()    const { return m_rotat; }
+    /// スケールを返す
     const VX::Math::vec3&  GetScale()     const { return m_scale; }
+    /// 変換行列を返す
     const VX::Math::matrix GetTransformMatrix() const
     {
         return VX::Math::Translation(m_trans[0], m_trans[1], m_trans[2])
@@ -41,15 +51,26 @@ public:
     typedef std::map<std::string, VX::Math::vec2> Vec2Map;
     typedef std::map<std::string, float>         FloatMap;
     typedef std::map<std::string, RefPtr<const BufferImageData> > TextureMap;
+    /// Uniformマップを返す
     const Vec4Map&    GetUniformVec4 ()   const { return m_vec4s;  }
+    /// Uniformマップを返す
     const Vec3Map&    GetUniformVec3 ()   const { return m_vec3s;  }
+    /// Uniformマップを返す
     const Vec2Map&    GetUniformVec2 ()   const { return m_vec2s;  }
+    /// Uniformマップを返す
     const FloatMap&   GetUniformFloat()   const { return m_floats; }
+    /// Uniformテクスチャマップを返す
     const TextureMap& GetUniformTexture() const { return m_imgs;   }
 
     //--------------------------------------------------
     //Set
     //--------------------------------------------------
+    /**
+     * 移動量の設定
+     * @param x x
+     * @param y y
+     * @param z z
+     */
     bool SetTranslate(float x, float y, float z) {
         m_trans[0] = x;
         m_trans[1] = y;
@@ -57,52 +78,97 @@ public:
         return true;
     }
     
+    /**
+     * 回転量の設定
+     * @param x x(radian)
+     * @param y y(radian)
+     * @param z z(radian)
+     */
     bool SetRotate(float x, float y, float z) {
         m_rotat[0] = x;
         m_rotat[1] = y;
         m_rotat[2] = z;
         return true;
     }
-
+    
+    /**
+     * スケールの設定
+     * @param x x
+     * @param y y
+     * @param z z
+     */
     bool SetScale(float x, float y, float z) {
         m_scale[0] = x;
         m_scale[1] = y;
         m_scale[2] = z;
         return true;
     }
-
+    
+    /**
+     * Unifrom値の設定
+     * @param name Uniform名
+     * @param x x
+     * @param y y
+     * @param z z
+     * @param w w
+     */
     bool SetVec4(const std::string& name, float x, float y, float z, float w) {
         m_vec4s[name] = VX::Math::vec4(x,y,z,w);
         return true;
     }
-
+    
+    /**
+     * Unifrom値の設定
+     * @param name Uniform名
+     * @param x x
+     * @param y y
+     * @param z z
+     */
     bool SetVec3(const std::string& name, float x, float y, float z) {
         m_vec3s[name] = VX::Math::vec3(x,y,z);
         return true;
     }
-
+    
+    /**
+     * Unifrom値の設定
+     * @param name Uniform名
+     * @param x x
+     * @param y y
+     */
     bool SetVec2(const std::string& name, float x, float y) {
         m_vec2s[name] = VX::Math::vec2(x,y);
         return true;
     }
-
+    
+    /**
+     * Unifrom値の設定
+     * @param name Uniform名
+     * @param x x
+     */
     bool SetFloat(const std::string& name, float x) {
         m_floats[name] = x;
         return true;
     }
     
+    /**
+     * テクスチャの設定
+     * @param name Uniform名
+     * @param img イメージデータ
+     */
     bool SetTexture(const std::string& name, const BufferImageData* img) {
         m_imgs[name] = img;
         return true;
     }
 
-    
+    /// コンストラクタ
     Impl(MODE_TYPE t) : m_type(t)
     {
         m_trans =  VX::Math::vec3(0, 0, 0);
         m_rotat =  VX::Math::vec3(0, 0, 0);
         m_scale =  VX::Math::vec3(1, 1, 1);
     };
+    
+    /// デストラクタ
     ~Impl() {};
     
 private:
@@ -123,35 +189,97 @@ private:
     RenderObject::MODE_TYPE m_type;
 };
 
+/// コンストラクタ
 RenderObject::RenderObject(MODE_TYPE t) : m_imp(new Impl(t)) {}
+/// デストラクタ
 RenderObject::~RenderObject() { delete m_imp; }
 
+/// モードタイプを返す.
 RenderObject::MODE_TYPE RenderObject::GetType() const { return m_imp->GetType();       }
+/// タイプ文字列を返す.
 std::string RenderObject::GetTypeString()       const { return m_imp->GetTypeString(); }
 
 //--------------------------------------------------
 //Get
 //--------------------------------------------------
+/// 移動量を返す
 const VX::Math::vec3&  RenderObject::GetTranslate()               const { return m_imp->GetTranslate();       }
+/// 回転量を返す
 const VX::Math::vec3&  RenderObject::GetRotate()                  const { return m_imp->GetRotate();          }
+/// スケールを返す
 const VX::Math::vec3&  RenderObject::GetScale()                   const { return m_imp->GetScale();           }
+/// 変換行列を返す
 const VX::Math::matrix RenderObject::GetTransformMatrix()         const { return m_imp->GetTransformMatrix(); }
+/// Uniformマップを返す
 const RenderObject::Vec4Map&  RenderObject::GetUniformVec4 ()     const { return m_imp->GetUniformVec4();     }
+/// Uniformマップを返す
 const RenderObject::Vec3Map&  RenderObject::GetUniformVec3 ()     const { return m_imp->GetUniformVec3();     }
+/// Uniformマップを返す
 const RenderObject::Vec2Map&  RenderObject::GetUniformVec2 ()     const { return m_imp->GetUniformVec2();     }
+/// Uniformマップを返す
 const RenderObject::FloatMap& RenderObject::GetUniformFloat()     const { return m_imp->GetUniformFloat();    }
+/// Uniformテクスチャマップを返す
 const RenderObject::TextureMap& RenderObject::GetUniformTexture() const { return m_imp->GetUniformTexture();  }
 
 //--------------------------------------------------
 //Set
 //--------------------------------------------------
+/**
+ * 移動量の設定
+ * @param x x
+ * @param y y
+ * @param z z
+ */
 bool RenderObject::SetTranslate(float x, float y, float z)                              { return m_imp->SetTranslate(x, y, z);     }
+/**
+ * 回転量の設定
+ * @param x x(radian)
+ * @param y y(radian)
+ * @param z z(radian)
+ */
 bool RenderObject::SetRotate(float x, float y, float z)                                 { return m_imp->SetRotate(x, y, z);        }
+/**
+ * スケールの設定
+ * @param x x
+ * @param y y
+ * @param z z
+ */
 bool RenderObject::SetScale(float x, float y, float z)                                  { return m_imp->SetScale(x, y, z);         }
+/**
+ * Unifrom値の設定
+ * @param name Uniform名
+ * @param x x
+ * @param y y
+ * @param z z
+ * @param w w
+ */
 bool RenderObject::SetVec4(const std::string& name, float x, float y, float z, float w) { return m_imp->SetVec4(name, x, y, z, w); }
+/**
+ * Unifrom値の設定
+ * @param name Uniform名
+ * @param x x
+ * @param y y
+ * @param z z
+ */
 bool RenderObject::SetVec3(const std::string& name, float x, float y, float z)          { return m_imp->SetVec3(name, x, y, z);    }
+/**
+ * Unifrom値の設定
+ * @param name Uniform名
+ * @param x x
+ * @param y y
+ */
 bool RenderObject::SetVec2(const std::string& name, float x, float y)                   { return m_imp->SetVec2(name, x, y);       }
+/**
+ * Unifrom値の設定
+ * @param name Uniform名
+ * @param x x
+ */
 bool RenderObject::SetFloat(const std::string& name, float x)                           { return m_imp->SetFloat(name, x);         }
+/**
+ * テクスチャの設定
+ * @param name Uniform名
+ * @param img イメージデータ
+ */
 bool RenderObject::SetTexture(const std::string& name, const BufferImageData* img)      { return m_imp->SetTexture(name, img);     }
 
 
