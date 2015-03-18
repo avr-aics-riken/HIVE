903bf15c0089dc4f9bfb5f5ec062c0a31eb82c0d
KazumaHatta <tori31001@gmail.com>
2015-03-18 15:35:16 +0900

    add doxygen comment

--- hrender/src/RenderObject/Camera.h
+++ hrender/src/RenderObject/Camera.h
@@ -1,3 +1,7 @@
+/**
+ * @file Camera.h
+ * カメラ
+ */
 #ifndef _CAMERA_H_
 #define _CAMERA_H_
 
@@ -5,9 +9,15 @@
 
 class BufferImageData;
 
+/**
+ * カメラ.
+ */
 class Camera : public RenderObject {
     
 public:
+    /**
+     * カメラ情報.
+     */
     struct CameraInfo {
         float eye[3];
         float tar[3];
@@ -15,16 +25,26 @@ public:
         float fov;
     };
     
+    /// クリアカラーを返す.
     const float*       GetClearColor() const;
+    /// スクリーン幅を返す.
     int                GetScreenWidth() const;
+    /// スクリーン高さを返す.
     int                GetScreenHeight() const;
+    /// カメラ情報を返す.
     const CameraInfo*  GetCameraInfo() const;
+    /// 出力ファイルパスを返す.
     const std::string& GetOutputFile() const;
+    /// 画像バッファを返す.
     BufferImageData*   GetImageBuffer();
+    /// 深度バッファを返す.
     BufferImageData*   GetDepthBuffer();
 
+    /// 位置を返す
     VX::Math::vec3 GetPosition() const;
+    /// ターゲットを返す
     VX::Math::vec3 GetTarget() const;
+    /// アップベクトルを返す
     VX::Math::vec3 GetUp() const;
     
 private:
@@ -34,15 +54,48 @@ private:
 protected:
     // Script Access Only
 	Camera();
-	~Camera();
-	bool SetScreenSize(unsigned int width, unsigned int height);
+    ~Camera();
+    
+    /**
+     * スクリーンサイズの設定.
+     * @param width 幅
+     * @param height 高さ
+     */
+    bool SetScreenSize(unsigned int width, unsigned int height);
+    
+    /**
+     * 結果画像のファイルパスの設定.
+     * @param filename ファイルパス
+     */
 	bool SetFilename(const std::string& outputfile);
     
+    /**
+     * 視線の設定.
+     * @param eye_x 視点ベクトルx
+     * @param eye_y 視点ベクトルy
+     * @param eye_z 視点ベクトルz
+     * @param tar_x ターゲットベクトルx
+     * @param tar_y ターゲットベクトルy
+     * @param tar_z ターゲットベクトルz
+     * @param up_x アップベクトルx
+     * @param up_y アップベクトルy
+     * @param up_z アップベクトルz
+     * @param fov 縦方向視野角
+     */
 	bool LookAt(float eye_x, float eye_y, float eye_z,
 				float tar_x, float tar_y, float tar_z,
 				float up_x,  float up_y,  float up_z,
 				float fov);
+    
+    /**
+     * クリアカラーの設定.
+     * @param red 赤
+     * @param green 緑
+     * @param blue 青
+     * @param alpha アルファ
+     */
     bool ClearColor(float red, float green, float blue, float alpha);
+    /// 視野角を返す
     float GetFov() const;
 };
 
