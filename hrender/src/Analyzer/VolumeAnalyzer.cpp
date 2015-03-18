d30f859a2732554a99d80a9a226f8ebba428805e
Masaki <masakisasaki@gmail.com>
2015-03-18 10:50:48 +0900

    Added doxygen comments Analyzer

--- hrender/src/Analyzer/VolumeAnalyzer.cpp
+++ hrender/src/Analyzer/VolumeAnalyzer.cpp
@@ -1,46 +1,84 @@
+/**
+ * @file VolumeAnalyzer.cpp
+ * ボリュームデータ解析モジュール
+ */
+
 #include "VolumeAnalyzer.h"
 #include "Analyzer.h"
 #include "../Buffer/BufferMeshData.h"
 #include "Buffer.h"
 
+/// コンストラクタ
 VolumeAnalyzer::VolumeAnalyzer()
 {
     Clear();
 }
 
+/// デストラクタ
 VolumeAnalyzer::~VolumeAnalyzer()
 {
 }
 
+/// メンバクリア
 void VolumeAnalyzer::Clear() {
     m_minVal[0] = m_minVal[1] = m_minVal[2] = 0.0;
     m_maxVal[0] = m_maxVal[1] = m_maxVal[2] = 0.0;
 }
 
+/**
+ * 解析したボリュームデータX軸最小値
+ * @return X軸最小値
+ */
 double VolumeAnalyzer::MinX() {
     return m_minVal[0];
 }
 
+/**
+ * 解析したボリュームデータX軸最小値
+ * @return X軸最小値
+ */
 double VolumeAnalyzer::MinY() {
     return m_minVal[1];
 }
 
+/**
+ * 解析したボリュームデータX軸最小値
+ * @return X軸最小値
+ */
 double VolumeAnalyzer::MinZ() {
     return m_minVal[2];
 }
 
+/**
+ * 解析したボリュームデータX軸最大値
+ * @return X軸最大値
+ */
 double VolumeAnalyzer::MaxX() {
     return m_maxVal[0];
 }
 
+/**
+ * 解析したボリュームデータY軸最大値
+ * @return Y軸最大値
+ */
 double VolumeAnalyzer::MaxY() {
     return m_maxVal[1];
 }
 
+/**
+ * 解析したボリュームデータZ軸最大値
+ * @return Z軸最大値
+ */
 double VolumeAnalyzer::MaxZ() {
     return m_maxVal[2];
 }
 
+/**
+ * ボリュームモデル解析
+ * @param model 解析対象PolygonModel
+ * @retval true 成功
+ * @retval false 失敗
+ */
 bool VolumeAnalyzer::Execute(VolumeModel *model)
 {
     VolumeAnalyzerProc proc;
