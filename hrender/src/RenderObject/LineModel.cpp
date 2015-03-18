75a65430ed6040e266030a89c75f932e8d378e92
KazumaHatta <tori31001@gmail.com>
2015-03-18 15:54:35 +0900

    add doxygen comment

--- hrender/src/RenderObject/LineModel.cpp
+++ hrender/src/RenderObject/LineModel.cpp
@@ -1,3 +1,7 @@
+/**
+ * @file LineModel.cpp
+ * ラインモデル
+ */
 #include <string>
 #include "LineModel.h"
 #include "BufferLineData.h"
@@ -5,43 +9,71 @@
 class LineModel::Impl
 {
 public:
+    
+    /// コンストラクタ
     Impl()
     {
         m_line = 0;
         m_width = 1.0f;
     }
+    
+    /// デストラクタ
     ~Impl()
     {
         
     }
 
+    /**
+     * シェーダファイルパスの設定.
+     * @param shaderfile シェーダファイルパス.
+     */
     bool SetShader(const std::string& shaderfile)
     {
         m_shaderfile = shaderfile;
         return true;
     }
     
+    /**
+     * ライン幅の設定
+     * @param width ライン幅
+     */
     bool SetLineWidth(float width)
     {
         m_width = width;
         return true;
     }
     
+    /**
+     * ラインデータの生成
+     * @param Linedata ラインデータ
+     */
     bool Create(BufferLineData* Linedata)
     {
         m_line = Linedata;
         return true;
     }
     
+    /**
+     * ラインデータの取得
+     * @retval ラインデータ
+     */
     BufferLineData* GetLine() const {
         return m_line;
     }
     
+    /**
+     * シェーダパスの取得
+     * @retval シェーダパス
+     */
     const std::string& GetShader() const
     {
         return m_shaderfile;
     }
 
+    /**
+     * ライン幅の取得
+     * @retval ライン幅
+     */
     float GetLineWidth() const
     {
         return m_width;
@@ -53,41 +85,68 @@ private:
     std::string m_shaderfile;
 
 };
+
+/// コンストラクタ
 LineModel::LineModel() : RenderObject(TYPE_LINE)
 {
     m_imp = new Impl();
 }
 
+/// デストラクタ
 LineModel::~LineModel()
 {
     delete m_imp;
 }
 
+/**
+ * シェーダファイルパスの設定.
+ * @param shaderfile シェーダファイルパス.
+ */
 bool LineModel::SetShader(const std::string& shaderfile)
 {
     return m_imp->SetShader(shaderfile);
 }
 
+/**
+ * ライン幅の設定.
+ * @param width ライン幅
+ */
 bool LineModel::SetLineWidth(float width)
 {
     return m_imp->SetLineWidth(width);
 }
 
+/**
+ * ラインデータの生成.
+ * @param Linedata ラインデータ
+ */
 bool LineModel::Create(BufferLineData* Linedata)
 {
     return m_imp->Create(Linedata);
 }
 
+/**
+ * ラインデータの取得.
+ * @retval ラインデータ
+ */
 BufferLineData* LineModel::GetLine() const
 {
     return m_imp->GetLine();
 }
 
+/**
+ * シェーダパスの取得.
+ * @retval シェーダパス
+ */
 const std::string& LineModel::GetShader() const
 {
     return m_imp->GetShader();
 }
 
+/**
+ * ライン幅の取得.
+ * @retval ライン幅
+ */
 float LineModel::GetLineWidth() const
 {
     return m_imp->GetLineWidth();
