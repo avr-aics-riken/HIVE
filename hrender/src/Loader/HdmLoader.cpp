4c48412f10385ee932940ca467a614694794b2b6
Syoyo Fujita <syoyo@lighttransport.com>
2015-05-06 17:21:12 +0900

    Support vector volume type for HdmLoader. Implement `Sample` method for SparseVolume.

--- hrender/src/Loader/HdmLoader.cpp
+++ hrender/src/Loader/HdmLoader.cpp
@@ -202,6 +202,67 @@ void LoadBlockCellScalar(VoxelBlock<T>& block, Scalar3D<T>* mesh, Vec3i sz, size
 }
 
 template <typename T>
+void LoadBlockCellVector(VoxelBlock<T>& block, Scalar3D<T>* U, Scalar3D<T>* V, Scalar3D<T>* W, Vec3i sz, size_t level, size_t maxLevel, size_t rootDim[3], const Vec3d& org, const Vec3d& globalOrigin, const Vec3d& pitch, const Vec3d& globalRegion) {
+
+	// Assume U, V and W has all same data layout.
+	T* dataU = U->getData();
+	T* dataV = V->getData();
+	T* dataW = W->getData();
+
+	Index3DS idx = U->getIndex();
+
+	// Compute voxel size in global voxel resolutuon
+	size_t dx = sz.x * (1 << (maxLevel - level));
+	size_t dy = sz.y * (1 << (maxLevel - level));
+	size_t dz = sz.z * (1 << (maxLevel - level));
+
+	size_t bx = (1 << (maxLevel - level));
+	size_t by = (1 << (maxLevel - level));
+	size_t bz = (1 << (maxLevel - level));
+
+	size_t ox = sz.x * rootDim[0] * ((1 << (maxLevel - level)) * ((double)(org.x - globalOrigin.x) / (double)globalRegion.x));
+	size_t oy = sz.y * rootDim[1] * ((1 << (maxLevel - level)) * ((double)(org.y - globalOrigin.y) / (double)globalRegion.y));
+	size_t oz = sz.z * rootDim[2] * ((1 << (maxLevel - level)) * ((double)(org.z - globalOrigin.z) / (double)globalRegion.z));
+	ox = (std::min)(rootDim[0] * dx, ox);
+	oy = (std::min)(rootDim[1] * dy, oy);
+	oz = (std::min)(rootDim[2] * dz, oz);
+	//printf("lv = %d\n", level);
+	//printf("org = %f, %f, %f\n", org.x, org.y, org.z);
+	//printf("pitch = %f, %f, %f\n", pitch.x, pitch.y, pitch.z);
+	//printf("o = %d, %d, %d, d = %d, %d, %d\n", ox, oy, oz, dx, dy, dz);
+	//printf("%f\n", rootDim[0] * sz.x * (1 << (maxLevel - level)) * (double)(org.x - globalOrigin.x) / (double)globalRegion.x);
+
+	block.Resize(dx, dy, dz);
+	block.offset[0] = ox;
+	block.offset[1] = oy;
+	block.offset[2] = oz;
+
+	// NOTE: Skip virtual Cell
+	for(size_t z = 0; z < sz.z; z++){
+		for(size_t y = 0; y < sz.y; y++){
+			for(size_t x = 0; x < sz.x; x++){
+				T valU = dataU[idx(x, y, z)];
+				T valV = dataV[idx(x, y, z)];
+				T valW = dataW[idx(x, y, z)];
+
+				// Splat voxel value
+				for (size_t w = 0; w < bz; w++) {
+					for (size_t v = 0; v < by; v++) {
+						for (size_t u = 0; u < bx; u++) {
+							block.data[3 * ((z * bz + w) * dy * dx + (y * by + v) * dx + (x * bx + u)) + 0] = valU;
+							block.data[3 * ((z * bz + w) * dy * dx + (y * by + v) * dx + (x * bx + u)) + 0] = valV;
+							block.data[3 * ((z * bz + w) * dy * dx + (y * by + v) * dx + (x * bx + u)) + 0] = valW;
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+}
+
+template <typename T>
 void ConvertLeafBlockScalar(BufferSparseVolumeData& sparseVolume, const int dcid, size_t maxLevel, size_t rootDim[3], const Vec3d& globalOrigin, const Vec3d& globalRegion){
     BlockManager& blockManager = BlockManager::getInstance();
     const MPI::Intracomm& comm = blockManager.getCommunicator();
@@ -213,9 +274,7 @@ void ConvertLeafBlockScalar(BufferSparseVolumeData& sparseVolume, const int dcid
 		size_t level = block->getLevel();
         const Vec3d& org = block->getOrigin();
         const Vec3d& pitch = block->getCellSize();
-		//printf("lv    = %d\n", level);
-		//printf("org   = %f, %f, %f\n", org.x, org.y, org.z);
-		//printf("pitch = %f, %f, %f\n", pitch.x, pitch.y, pitch.z);
+
         Scalar3D<T> *mesh = dynamic_cast< Scalar3D<T>* >(block->getDataClass(dcid));
 
 		VoxelBlock<T> vb;
@@ -227,6 +286,39 @@ void ConvertLeafBlockScalar(BufferSparseVolumeData& sparseVolume, const int dcid
 
 		// Implicitly convert voxel data to float precision if T is double.
 		for (size_t i = 0; i < vb.size[0] * vb.size[1] * vb.size[2]; i++) {
+			vol->Buffer()->GetBuffer()[i] = vb.data[i];
+		}
+
+		sparseVolume.AddVolume(vb.offset[0], vb.offset[1], vb.offset[2], vol);
+    }
+}
+
+template <typename T>
+void ConvertLeafBlockVector(BufferSparseVolumeData& sparseVolume, const int dcid[3], size_t maxLevel, size_t rootDim[3], const Vec3d& globalOrigin, const Vec3d& globalRegion){
+    BlockManager& blockManager = BlockManager::getInstance();
+    const MPI::Intracomm& comm = blockManager.getCommunicator();
+    Vec3i sz = blockManager.getSize();
+
+    for(int id = 0; id < blockManager.getNumBlock(); ++id){
+        BlockBase* block = blockManager.getBlock(id);
+
+		size_t level = block->getLevel();
+        const Vec3d& org = block->getOrigin();
+        const Vec3d& pitch = block->getCellSize();
+
+        Scalar3D<T> *meshU = dynamic_cast< Scalar3D<T>* >(block->getDataClass(dcid[0]));
+        Scalar3D<T> *meshV = dynamic_cast< Scalar3D<T>* >(block->getDataClass(dcid[1]));
+        Scalar3D<T> *meshW = dynamic_cast< Scalar3D<T>* >(block->getDataClass(dcid[2]));
+
+		VoxelBlock<T> vb;
+		LoadBlockCellVector(vb, meshU, meshV, meshW, sz, level, maxLevel, rootDim, org, globalOrigin, pitch, globalRegion);
+		vb.id = id;
+
+		BufferVolumeData* vol = new BufferVolumeData();
+		vol->Create(vb.size[0], vb.size[1], vb.size[2], /* components */ 3);
+
+		// Implicitly convert voxel data to float precision if T is double.
+		for (size_t i = 0; i < vb.size[0] * vb.size[1] * vb.size[2] * 3; i++) {
 			vol->Buffer()->GetBuffer()[i] = vb.data[i];
 		}
 
@@ -338,19 +430,22 @@ bool HDMLoader::Load(const char* cellidFilename, const char* dataFilename, const
 				ConvertLeafBlockScalar<float>(m_sparseVolume, id_s32, maxLevel, rootDim, globalOrigin, globalRegion);
 			} else {
 				loader.LoadLeafBlock(id_v32, fieldName, vc, *it);
+				ConvertLeafBlockVector<float>(m_sparseVolume, id_v32, maxLevel, rootDim, globalOrigin, globalRegion);
 			}
 
 		} else if (type == TYPE_FLOAT64) {
 			if (components == 1) {
 				loader.LoadLeafBlock(&id_s64, fieldName, vc, *it);
-				//print<float>(prefix, id_s64, vc);
+				ConvertLeafBlockScalar<double>(m_sparseVolume, id_s64, maxLevel, rootDim, globalOrigin, globalRegion);
 			} else {
 				loader.LoadLeafBlock(id_v64, fieldName, vc, *it);
+				ConvertLeafBlockVector<double>(m_sparseVolume, id_v64, maxLevel, rootDim, globalOrigin, globalRegion);
 			}
 		}
 
 	}
 
+	// @note { Must call `Build` here to build spacial data structure for sparse volume(to use `Sample` meethod in later phase. }
 	m_sparseVolume.Build();
 
 	return true;
@@ -387,14 +482,6 @@ int HDMLoader::Depth()    {
 int HDMLoader::Component() {
     return m_sparseVolume.Component();
 }
-
-/**
- * HDMデータバッファ取得
- * @retval FloatBuffer* HDMデータバッファアドレス
- */
-//FloatBuffer* HDMLoader::Buffer() {
-//    return m_sparseVolume.Buffer();
-//}
 
 /**
  * SparseVolumeData取得
