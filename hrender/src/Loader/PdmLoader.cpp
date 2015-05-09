d7dbcddbd9fef8537c88ce7802ed8116eefdd4c4
Syoyo Fujita <syoyo@lighttransport.com>
2015-05-09 16:36:08 +0900

    Support loading custom attribute from PDMlib.

--- hrender/src/Loader/PdmLoader.cpp
+++ hrender/src/Loader/PdmLoader.cpp
@@ -21,7 +21,12 @@ PDMLoader::~PDMLoader(){};
 /// メンバクリア
 void PDMLoader::Clear()
 {
-	points.Clear();
+	m_timeStep = -1;
+	m_readAll = false;
+	m_pointMap.clear();
+	m_vec3Map.clear();
+	m_floatMap.clear();
+	m_uintMap.clear();
 }
 
 /**
@@ -35,8 +40,6 @@ void PDMLoader::Clear()
 bool PDMLoader::Load(const char* filename, int timeStep){
 	Clear();
 
-
-	printf("CDM load\n");
 	std::string fname = std::string(filename);
 
 	// Its OK to pass argc=0,argv=NULL since its only used to initialize MPI inside of Zoltan lib(called from PDMlib::Init()).
@@ -44,45 +47,142 @@ bool PDMLoader::Load(const char* filename, int timeStep){
 	PDMlib::PDMlib::GetInstance().Init(0, NULL, /* write filename = */"",  fname);
 
 	std::vector<PDMlib::ContainerInfo>& infos = PDMlib::PDMlib::GetInstance().GetContainerInfo();
-	printf("# of infos = %d\n", infos.size());
+	printf("[PDMloader] # of infos = %d\n", infos.size());
 	for (size_t i = 0; i < infos.size(); i++) {
-		printf("[%d] name = %s, type = %d, ncomp = %d, order = %d, suffix = %s, annotation = %s, compression = %s\n",
+		printf("[PDMLoader] [%d] name = %s, type = %d, ncomp = %d, order = %d, suffix = %s, annotation = %s, compression = %s\n",
 				i, infos[i].Name.c_str(), infos[i].Type, infos[i].nComp, infos[i].VectorOrder, infos[i].Suffix.c_str(), infos[i].Annotation.c_str(), infos[i].Compression.c_str());
 	}
 
-	int* ID = NULL;
-	float* Coordinate = NULL;
+	m_timeStep = timeStep;
+	m_readAll = false; // @fixme
 
-	//PDMlib::PDMlib::GetInstance().RegisterContainer("ParticleID", &ID);
-	//PDMlib::PDMlib::GetInstance().RegisterContainer("Coordinate", &Coordinate);
+	// NOTE: Reading attributes are done with subsequence call of ReadFloat(), ReadVec3(), ...
 
-	size_t CoordinateLen = 0;
-	bool readall = false; // @fixme
-	{
-		int ret = PDMlib::PDMlib::GetInstance().Read("Coordinate", &CoordinateLen, &Coordinate, &timeStep, readall);
-	}
-
-	// Alloc
-	{
-		printf("[PDMlib] CoorinateLen = %d\n", CoordinateLen);
-		points.Create(CoordinateLen);
-
-		std::copy(Coordinate, Coordinate + CoordinateLen, points.Position()->GetBuffer());
-	}
-
-	delete [] Coordinate;
-
-	// @todo { Read custom attributes. }
-
-	// @todo {}
-	return false;
+	return true;
 }
 
 /**
  * 点データ取得
  * @retval BufferPointData* 点データバッファへの参照
+ * @param containerName name of point data container
+ * @param radius Radius of point
  */
-BufferPointData *PDMLoader::PointData()
+BufferPointData *PDMLoader::PointData(const char* containerName, float radius)
 {
-	return &points;
+	if (m_pointMap.find(containerName) != m_pointMap.end()) {
+		return m_pointMap[containerName];
+	}
+
+	float* points = NULL;
+	size_t n = 0;
+	int ret = PDMlib::PDMlib::GetInstance().Read(containerName, &n, &points, &m_timeStep, m_readAll);
+
+	if (n == 0 || points == NULL) {
+		fprintf(stderr, "[CDMLoader] No such a container with point type: %s\n", containerName);
+		return NULL;
+	}
+
+	printf("[PDMloader] %s : n = %d\n", containerName, n);
+
+	// Alloc
+	BufferPointData* buf = new BufferPointData();
+	buf->Create(n/3);
+	std::copy(points, points + n, buf->Position()->GetBuffer());
+
+	printf("[PDMloader] radius = %f\n", radius);
+
+	std::fill(buf->Radius()->GetBuffer(), buf->Radius()->GetBuffer() + (n/3), radius);
+
+	m_pointMap[containerName] = buf;
+
+	delete [] points; // Release memory allocated by PDMlib 
+	
+	return m_pointMap[containerName];
+}
+
+Vec3Buffer *PDMLoader::Vec3Data(const char* containerName)
+{
+	if (m_vec3Map.find(containerName) != m_vec3Map.end()) {
+		return m_vec3Map[containerName];
+	}
+
+	float* values = NULL;
+	size_t n = 0;
+	int ret = PDMlib::PDMlib::GetInstance().Read(containerName, &n, &values, &m_timeStep, m_readAll);
+
+	if (n == 0 || values == NULL) {
+		fprintf(stderr, "[CDMLoader] No such a container with point type: %s\n", containerName);
+		return NULL;
+	}
+
+	printf("[PDMloader] %s : n = %d\n", containerName, n);
+
+	// Alloc
+	Vec3Buffer* buf = new Vec3Buffer();
+	buf->Create(n/3);
+	std::copy(values, values + n, buf->GetBuffer());
+
+	m_vec3Map[containerName] = buf;
+
+	delete [] values; // Release memory allocated by PDMlib 
+	
+	return m_vec3Map[containerName];
+}
+
+FloatBuffer *PDMLoader::FloatData(const char* containerName)
+{
+	if (m_floatMap.find(containerName) != m_floatMap.end()) {
+		return m_floatMap[containerName];
+	}
+
+	float* values = NULL;
+	size_t n = 0;
+	int ret = PDMlib::PDMlib::GetInstance().Read(containerName, &n, &values, &m_timeStep, m_readAll);
+
+	if (n == 0 || values == NULL) {
+		fprintf(stderr, "[CDMLoader] No such a container with point type: %s\n", containerName);
+		return NULL;
+	}
+
+	printf("[PDMloader] %s : n = %d\n", containerName, n);
+
+	// Alloc
+	FloatBuffer* buf = new FloatBuffer();
+	buf->Create(n);
+	std::copy(values, values + n, buf->GetBuffer());
+
+	m_floatMap[containerName] = buf;
+
+	delete [] values; // Release memory allocated by PDMlib 
+	
+	return m_floatMap[containerName];
+}
+
+UintBuffer *PDMLoader::UintData(const char* containerName)
+{
+	if (m_uintMap.find(containerName) != m_uintMap.end()) {
+		return m_uintMap[containerName];
+	}
+
+	unsigned int * values = NULL;
+	size_t n = 0;
+	int ret = PDMlib::PDMlib::GetInstance().Read(containerName, &n, &values, &m_timeStep, m_readAll);
+
+	if (n == 0 || values == NULL) {
+		fprintf(stderr, "[CDMLoader] No such a container with point type: %s\n", containerName);
+		return NULL;
+	}
+
+	printf("[PDMloader] %s : n = %d\n", containerName, n);
+
+	// Alloc
+	UintBuffer* buf = new UintBuffer();
+	buf->Create(n);
+	std::copy(values, values + n, buf->GetBuffer());
+
+	m_uintMap[containerName] = buf;
+
+	delete [] values; // Release memory allocated by PDMlib 
+	
+	return m_uintMap[containerName];
 }
