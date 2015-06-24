fc94afb798d036d22f36257f35b79236e862c195
kioku <kentaro-o@digirea.com>
2015-06-24 15:18:39 +0900

    add new nodes

--- HIVE/SceneNodeEditor/nodes/CreateVectorModel/CreateVectorModel.lua
+++ HIVE/SceneNodeEditor/nodes/CreateVectorModel/CreateVectorModel.lua
@@ -12,19 +12,19 @@ function CreateVectorModel(property)
 	for i,v in pairs(uniforms) do
 		if v.type == 'vec4' then
 			print('vec4[' .. v.name .. '] = (', v.value[1], v.value[2], v.value[3], v.value[4], ')')
-			lm:SetVec4(v.name, v.value[1], v.value[2], v.value[3], v.value[4])
+			vm:SetVec4(v.name, v.value[1], v.value[2], v.value[3], v.value[4])
 		end
 		if v.type == 'vec3' then
 			print('vec3[' .. v.name .. '] = (', v.value[1], v.value[2], v.value[3], ')')
-			lm:SetVec3(v.name, v.value[1], v.value[2], v.value[3])
+			vm:SetVec3(v.name, v.value[1], v.value[2], v.value[3])
 		end
 		if v.type == 'vec2' then
 			print('vec2[' .. v.name .. '] = (', v.value[1], v.value[2], ')')
-			lm:SetVec2(v.name, v.value[1], v.value[2])
+			vm:SetVec2(v.name, v.value[1], v.value[2])
 		end
 		if v.type == 'float' then
 			print('float[' .. v.name .. '] = (', v.value, ')')
-			lm:SetFloat(v.name, v.value)
+			vm:SetFloat(v.name, v.value)
 		end
 	end
 	function getModel()
