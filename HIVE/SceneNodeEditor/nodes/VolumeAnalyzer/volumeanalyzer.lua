d67c0ad84877172e25e63dfc5435f96707390da1
kioku <kentaro-o@digirea.com>
2015-06-25 17:06:36 +0900

    update SceneNodeEditor nodes.

---
+++ HIVE/SceneNodeEditor/nodes/VolumeAnalyzer/volumeanalyzer.lua
@@ -0,0 +1,9 @@
+function VolumeAnalizeFunc(arg)
+	-- TODO
+	print('TODO:VolumeAnalyzerFunc')
+	--execute(arg.model)
+	return {
+		min=function () return {0,0,0} end,
+		max=function () return {0,0,0} end
+	}
+end
