fb9bc45d161d005605123e0646d39188926aa5ff
Syoyo Fujita <syoyo@lighttransport.com>
2015-08-03 17:03:34 +0900

    Fix HEXA-8 conversion.

--- hrender/src/Loader/UdmLoader.cpp
+++ hrender/src/Loader/UdmLoader.cpp
@@ -344,17 +344,17 @@ bool UDMLoader::Load(const char* filename, int timeStepNo)
 				//
 				// Hexa cube with shared vertex.
 				const int idx [] = {
-					0, 2, 1, 1, 2, 3,	// -Y(bottom)
-					1, 3, 5, 3, 7, 5,   // +X
-					5, 7, 6, 5, 6, 4,   // +Y(top)
-					4, 6, 2, 4, 2, 0,   // -X
+					0, 2, 1, 0, 3, 2,	// -Y(bottom)
+					1, 2, 5, 2, 6, 5,   // +X
+					5, 6, 7, 5, 7, 4,   // +Y(top)
+					4, 7, 3, 4, 3, 0,   // -X
 					0, 1, 5, 0, 5, 4,   // +Z
-					6, 7, 3, 6, 3, 2    // -Z
+					6, 2, 3, 6, 3, 7    // -Z
 				};
 
 				size_t indexOffset = trianglePoints.size() / 3;
 				for (int i = 0; i < 36; i++) {
-					triangleIndices.push_back(indexOffset + i);
+					triangleIndices.push_back(indexOffset + idx[i]);
 				}
 				trianglePoints.insert(trianglePoints.end(), points.begin(), points.end());
 			} else {
@@ -376,7 +376,23 @@ bool UDMLoader::Load(const char* filename, int timeStepNo)
 	if ((trianglePoints.size() > 0) && (triangleIndices.size() > 0)) {
 		int numVertices = trianglePoints.size() / 3;
 		int numIndices = triangleIndices.size();
-		m_mesh = new BufferMeshData();
+		//printf("verts = %d\n", numVertices);
+		//printf("inds = %d\n", numIndices);
+		//m_mesh = new BufferMeshData();
+
+		//for (int x = 0; x < numIndices/3; x++) {
+		//	printf("inds[%d] = %d, %d, %d\n", x,
+		//					triangleIndices[3*x+0],
+		//					triangleIndices[3*x+1],
+		//					triangleIndices[3*x+2]);
+		//}
+
+		//for (int x = 0; x < numVertices; x++) {
+		//	printf("verts[%d] = %f, %f, %f\n", x,
+		//					trianglePoints[3*x+0],
+		//					trianglePoints[3*x+1],
+		//					trianglePoints[3*x+2]);
+		//}
 
 		m_mesh->Create(numVertices, numIndices);
 		Vec3Buffer* pos = m_mesh->Position();
