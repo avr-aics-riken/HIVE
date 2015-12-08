f18c301f894d862eac27cfe165356f9d41cec2ad
KazumaHatta <tori31001@gmail.com>
2015-12-08 22:47:54 +0900

    fix requires of tests

--- lib/Analyzer/LuaEntry.cpp
+++ lib/Analyzer/LuaEntry.cpp
@@ -8,8 +8,15 @@ int luaopen_Analyzer(lua_State* L)
 {
     LUA_SCRIPTCLASS_REGISTER(L, VolumeAnalyzer_Lua);
     LUA_SCRIPTCLASS_REGISTER(L, PolygonAnalyzer_Lua);
+    lua_newtable(L);
+    lua_pushstring(L, "VolumeAnalyzer");
     lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(VolumeAnalyzer_Lua));
+    lua_settable(L, -3);
+
+    lua_pushstring(L, "PolygonAnalyzer");
     lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(PolygonAnalyzer_Lua));
+    lua_settable(L, -3);
+
     return 1;
 }
 
--- lib/Network/LuaEntry.cpp
+++ lib/Network/LuaEntry.cpp
@@ -5,12 +5,19 @@
 extern "C" {
 
 
-int luaopen_Connection(lua_State* L)
+int luaopen_Network(lua_State* L)
 {
     LUA_SCRIPTCLASS_REGISTER(L, Connection_Lua);
     LUA_SCRIPTCLASS_REGISTER(L, MetaBinary_Lua);
+    lua_newtable(L);
+    lua_pushstring(L, "Connection"); 
     lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(Connection_Lua));
+    lua_settable(L, -3);
+
+    lua_pushstring(L, "MetaBinary"); 
     lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(MetaBinary_Lua));
+    lua_settable(L, -3);
+    
     return 1;
 }
 
