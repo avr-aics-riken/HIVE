92eb4eb34584c83ecfe71dd90045bdad77085353
kioku <kentaro-o@digirea.com>
2015-12-10 21:24:31 +0900

    update send ipc

--- include/SceneScript/BufferImageData_Lua.h
+++ include/SceneScript/BufferImageData_Lua.h
@@ -6,6 +6,7 @@
 #define _BUFFERIMAGEDATA_LUA_H_
 
 #include "BufferImageData.h"
+#include "Buffer.h"
 
 /**
  * BufferImageData Luaラッパー
@@ -16,11 +17,23 @@ public:
     BufferImageData_Lua(BufferImageData* data) : BufferImageData(data) { }
     BufferImageData_Lua(const BufferImageData* data) : BufferImageData(data) { }
     BufferImageData_Lua() {}
+    
+    int GetWidth() const { return Width(); };
+    int GetHeight() const { return Height(); };
+    //const char* GetFormat() const { return Format() };
+    int GetSize() const { return Bytes(); };
+    const char* GetBuffer() const { return reinterpret_cast<const char*>(ImageBuffer()->GetBuffer()); };
+
     ~BufferImageData_Lua() {}
     
 public:
     static BufferImageData_Lua* CreateInstance(BufferImageData* bufferImageData = 0);
     LUA_SCRIPTCLASS_BEGIN(BufferImageData_Lua)
+    LUA_SCRIPTCLASS_METHOD_ARG0(int, GetWidth)
+    LUA_SCRIPTCLASS_METHOD_ARG0(int, GetHeight)
+    //LUA_SCRIPTCLASS_METHOD_ARG0(const char*, GetFormat)
+    LUA_SCRIPTCLASS_METHOD_ARG0(int, GetSize)
+    LUA_SCRIPTCLASS_METHOD_ARG0(const char*, GetBuffer)
     LUA_SCRIPTCLASS_END();
 };
 LUA_SCRIPTCLASS_CAST_AND_PUSH(BufferImageData_Lua);
