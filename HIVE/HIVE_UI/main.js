d8d27d4a62bf7fb975fb8cee73be103e5a7a7fed
kioku <kentaro-o@digirea.com>
2015-12-14 15:55:30 +0900

    HIVE UI support electron

---
+++ HIVE/HIVE_UI/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+var server = require('./server');
+
+app.on('window-all-closed', function() {
+	app.quit();
+});
+
+app.on('ready', function() {
+
+	// ブラウザ(Chromium)の起動, 初期画面のロード
+	mainWindow = new BrowserWindow({width: 1280, height: 900});
+	mainWindow.loadURL('file://' + __dirname + '/root/index.html');
+
+	mainWindow.on('closed',
+		function() {
+			server.stopHRenderServer();
+			mainWindow = null;
+	    });
+    });
