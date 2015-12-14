97c9d5ea42100beeb91ed0695d1f766dd523f0fa
KazumaHatta <tori31001@gmail.com>
2015-12-14 20:30:25 +0900

    fix terminal scroll

--- tools/fiberui/js/term.js
+++ tools/fiberui/js/term.js
@@ -27,12 +27,13 @@
 
 		bash.on('data', (function (term) {
 			return function (data) {
-				var elem = target_element.getElementsByClassName('terminal')[0];
+				var elem = target_element.getElementsByClassName('terminal')[0],
+					div_height = term.geometry[1];
 				term.write(data);
 				// カーソル行が見た上画面一番下あたりに来るように、スクロール量を調整する.
 				// term.y はterminalカーソルの行数.
-				if ((term.y * term.geometry[1]) > elem.getBoundingClientRect().height) {
-					elem.scrollTop = (term.y + 1) * term.geometry[1] - elem.getBoundingClientRect().height;
+				if ((term.y + 1) * div_height > elem.getBoundingClientRect().height) {
+					elem.scrollTop = (term.y + 1) * div_height - elem.getBoundingClientRect().height;
 				}
 			};
 		}(term)));
