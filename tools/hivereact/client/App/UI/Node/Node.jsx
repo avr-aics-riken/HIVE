29555d7dc1cf4321841f5ef085ae32517801e932
KazumaHatta <tori31001@gmail.com>
2016-02-18 14:33:18 +0900

    separate files

--- tools/hivereact/client/App/UI/Node/Node.jsx
+++ tools/hivereact/client/App/UI/Node/Node.jsx
@@ -1,36 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import NodeInOut from "./NodeInOut.jsx"
 
-function colorFunction(type) {
-	if (type === "string") {
-		return "#14a271";
-	} else if (type === "float") {
-		return "#139aa5";
-	} else if (type === "vec4") {
-		return "#1b6ad6";
-	} else if (type === "vec3") {
-		return "#566f9f";
-	} else if (type === "vec2") {
-		return "#8222a7";
-	} else if (type === "RenderObject") {
-		return "#ad3b78";
-	} else if (type === "Uniform") {
-		return "#b19e14";
-	} else if (type === "BufferMeshData") {
-		return "#be1656";
-	} else if (type === "BufferPointData") {
-		return "#e023e0";
-	} else if (type === "BufferLineData") {
-		return "#20cae0";
-	} else if (type === "BufferVolumeData") {
-		return "#17d042";
-	} else if (type === "Any") {
-		return "#ef8815";
-	} else { // Object
-		return "#c12417";
-	}
-}
-
+/**
+ * ノード.
+ */
 export default class Node extends React.Component {
 	constructor(props) {
 		super(props);
@@ -45,7 +19,6 @@ export default class Node extends React.Component {
 		this.onMouseUp = this.onMouseUp.bind(this);
 		this.onMouseDown = this.onMouseDown.bind(this);
 		this.styles = this.styles.bind(this);
-		this.inoutStyle = this.inoutStyle.bind(this);
 	}
 
 	styles() {
@@ -63,59 +36,6 @@ export default class Node extends React.Component {
 			title : {
 				color : "rgb(239, 136, 21)",
 				fontSize : "16px"
-			}
-		}
-	}
-
-	inoutStyle(inout, key) {
-		return {
-			input : {
-				position : "absolute",
-				left : String(-15 / 2),
-				top : String(18 * (key + 1) + 10),
-				width : "100px",
-				height : "20px"
-			},
-			output : {
-				position : "absolute",
-				right : String(-15 / 2),
-				top : String(18 * (key + 1) + 10),
-				width : "100px",
-				height : "20px"
-			},
-			inhole : {
-				position : "absolute",
-				left : "0px",
-				width : "15px",
-				height : "15px",
-				marginTop : "3px",
-				borderRadius : "15px",
-				backgroundColor : colorFunction(inout.type)
-			},
-			outhole : {
-				position : "absolute",
-				right : "0px",
-				width : "15px",
-				height : "15px",
-				marginTop : "3px",
-				borderRadius : "15px",
-				backgroundColor : colorFunction(inout.type)
-			},
-			inholeText : {
-				position : "absolute",
-				top : "0px",
-				left : "15px", // holeのサイズ
-				marginLeft : "4px",
-				color : "white",
-				fontSize : "14px"
-			},
-			outholeText : {
-				position : "absolute",
-				top : "0px",
-				right : "15px", // holeのサイズ
-				marginRight : "4px",
-				color : "white",
-				fontSize : "14px"
 			}
 		}
 	}
@@ -152,36 +72,26 @@ export default class Node extends React.Component {
 		}
 	}
 
+	/// タイトル.
+	titleElem() {
+		const styles = this.styles();
+		return <div style={styles.title}>{this.props.node.name}</div>
+	}
+
 	/// 入力端子.
 	inputElem() {
-		const styles = this.styles();
 		let inputs = this.props.node.input.map( (inputData, key) => {
-			const inoutStyle = this.inoutStyle(inputData, key);
-			return (<div style={inoutStyle.input} key={key}>
-						<div style={inoutStyle.inhole} />
-						<div style={inoutStyle.inholeText}>{inputData.name}</div>
-					</div>)
+			return (<NodeInOut isInput={true} data={inputData} key={key} index={key} />)
 		});
 		return (<div>{inputs}</div>);
 	}
 
 	/// 出力端子.
 	outputElem() {
-		const styles = this.styles();
 		let outputs = this.props.node.output.map( (outputData, key) => {
-			const inoutStyle = this.inoutStyle(outputData, key);
-			return (<div style={inoutStyle.output} key={key}>
-						<div style={inoutStyle.outhole} />
-						<div style={inoutStyle.outholeText}>{outputData.name}</div>
-					</div>)
+			return (<NodeInOut isInput={false} data={outputData} key={key} index={key} />)
 		});
 		return (<div>{outputs}</div>);
-	}
-
-	/// タイトル.
-	titleElem() {
-		const styles = this.styles();
-		return <div style={styles.title}>{this.props.node.name}</div>
 	}
 
 	render () {
