f4f67ecc80b43caf74dc607ec5bb8623a4ad7b62
KazumaHatta <tori31001@gmail.com>
2016-02-18 12:38:34 +0900

    change node ui

--- tools/hivereact/client/App/UI/Node/Node.jsx
+++ tools/hivereact/client/App/UI/Node/Node.jsx
@@ -14,8 +14,8 @@ export default class Node extends React.Component {
 		this.onMouseMove = this.onMouseMove.bind(this);
 		this.onMouseUp = this.onMouseUp.bind(this);
 		this.onMouseDown = this.onMouseDown.bind(this);
-		this.inputs = [];
-		this.outputs = [];
+		this.styles = this.styles.bind(this);
+		this.inoutStyle = this.inoutStyle.bind(this);
 	}
 
 	styles() {
@@ -25,9 +25,67 @@ export default class Node extends React.Component {
 				left : String(this.state.pos[0]),
 				top : String(this.state.pos[1]),
 				width : "200px",
-				height : "100px",
-				backgroundColor : "red",
-				color : "white"
+				height : String((Math.max(this.props.node.input.length, this.props.node.output.length) + 1) * 18 + 10),
+				backgroundColor : "rgb(66, 69, 66)",
+				color : "white",
+				padding : "5px"
+			},
+			title : {
+				color : "rgb(239, 136, 21)",
+				fontSize : "16px"
+			},
+			inhole : {
+				position : "absolute",
+				left : "0px",
+				width : "15px",
+				height : "15px",
+				marginTop : "3px",
+				borderRadius : "15px",
+				backgroundColor : "red"
+			},
+			outhole : {
+				position : "absolute",
+				right : "0px",
+				width : "15px",
+				height : "15px",
+				marginTop : "3px",
+				borderRadius : "15px",
+				backgroundColor : "red"
+			},
+			inholeText : {
+				position : "absolute",
+				top : "0px",
+				left : "15px", // holeのサイズ
+				marginLeft : "4px",
+				color : "white",
+				fontSize : "14px"
+			},
+			outholeText : {
+				position : "absolute",
+				top : "0px",
+				right : "15px", // holeのサイズ
+				marginRight : "4px",
+				color : "white",
+				fontSize : "14px"
+			}
+		}
+	}
+
+	inoutStyle(key) {
+		return {
+			input : {
+				position : "absolute",
+				left : String(-15 / 2),
+				top : String(18 * (key + 1) + 10),
+				width : "100px",
+				height : "20px"
+			},
+			output : {
+				position : "absolute",
+				right : String(-15 / 2),
+				top : String(18 * (key + 1) + 10),
+				width : "100px",
+				height : "20px"
 			}
 		}
 	}
@@ -64,11 +122,42 @@ export default class Node extends React.Component {
 		}
 	}
 
+	inputElem() {
+		const styles = this.styles();
+		let inputs = this.props.node.input.map( (inputData, key) => {
+			return (<div style={this.inoutStyle(key).input} key={key}>
+						<div style={styles.inhole} />
+						<div style={styles.inholeText}>{inputData.name}</div>
+					</div>)
+		});
+		return (<div>{inputs}</div>);
+	}
+
+	outputElem() {
+		const styles = this.styles();
+		let outputs = this.props.node.output.map( (outputData, key) => {
+			return (<div style={this.inoutStyle(key).output} key={key}>
+						<div style={styles.outhole} />
+						<div style={styles.outholeText}>{outputData.name}</div>
+					</div>)
+		});
+		return (<div>{outputs}</div>);
+	}
+
+	titleElem() {
+		const styles = this.styles();
+		return <div style={styles.title}>{this.props.node.name}</div>
+	}
+
 	render () {
-		const styles = this.styles.bind(this)();
+		const styles = this.styles();
 		return (<div style={styles.node}
 					ref="node"
 					onMouseDown={this.onMouseDown.bind(this)}
-			>{this.props.node.name}</div>);
+				>
+					{this.titleElem.bind(this)()}
+					{this.inputElem.bind(this)()}
+					{this.outputElem.bind(this)()}
+				</div>);
 	}
 }
