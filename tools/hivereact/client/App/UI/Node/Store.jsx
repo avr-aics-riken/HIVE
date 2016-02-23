5864a2184000306104e7a81e004e07362092c016
KazumaHatta <tori31001@gmail.com>
2016-02-23 12:46:12 +0900

    add node files

---
+++ tools/hivereact/client/App/UI/Node/Store.jsx
@@ -0,0 +1,38 @@
+import EventEmitter from 'eventemitter3'
+import { Dispatcher } from "../../Core"
+
+export default class Store extends EventEmitter {
+	constructor() {
+		super();
+		this.dispatchToken = Dispatcher.register(this.actionHandler.bind(this));
+	}
+
+	/**
+	 * dispatchTokenを返す.
+	 */
+	getDispatchToken() {
+		return this.dispatchToken;
+	}
+
+	/**
+	 * アクションハンドラ
+	 * @private
+	 */
+	actionHandler(payload) {
+		if (payload && this.hasOwnProperty(payload.actionType)) {
+			if (payload.hasOwnProperty("id") && payload.id === this.dispatchToken) {
+				(() => {
+					this[payload.actionType].bind(this)(payload);
+				}());
+			}
+		}
+	}
+
+	/**
+	 * プラグ位置を変更する.
+	 * @private
+	 */
+	changePlugPosition(payload) {
+		console.log("change plug position", payload);
+	}
+}
