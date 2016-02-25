f0830a722bdea029cebfddf1cc5598daadd7c684
kioku <kentaro-o@digirea.com>
2016-02-25 12:36:28 +0900

    add addNodeByName in action

--- tools/hivereact/client/App/Core/Action.jsx
+++ tools/hivereact/client/App/Core/Action.jsx
@@ -1,9 +1,23 @@
 import Dispatcher from "./Dispatcher.jsx";
 
+function uuid() {
+    var uuid = "", i, random;
+    for (i = 0; i < 32; i++) {
+        random = Math.random() * 16 | 0;
+
+        if (i == 8 || i == 12 || i == 16 || i == 20) {
+            uuid += "_"
+        }
+        uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+    }
+    return uuid;
+}
+
 export default class HiveAction {
-	constructor(id) {
+	constructor(id, nodeSystem) {
 		this.dispatcher = Dispatcher;
 		this.id = id;
+        this.nodeSystem = nodeSystem;        
 	}
 
 	/**
@@ -16,6 +30,61 @@ export default class HiveAction {
 			actionType: "addNode",
 			nodeInfo : nodeInfo
 		});
+	}
+    
+    /**
+	 * ノードを追加する
+	 * @param nodeInfo ノード情報
+	 */
+	addNodeByName(nodeName) {
+        let node = this.nodeSystem.CreateNodeInstance(nodeName);
+        if (!node) {
+            return false;
+        }
+        
+		// create unique varname
+		node.varname += name + uuid();
+        /*for (let i = 0; true; i = i + 1) {
+			let foundSameName = false;
+			let name = node.varname + "_" + String(i);
+			for (let i = 0; i < this.state.nodes.length; i = i + 1) {
+				if (this.state.nodes[i].varname === name) {
+					foundSameName = true;
+					break;
+				}
+			}
+			if (!foundSameName) {
+				node.varname = name;
+				break;
+			}
+		}*/
+        
+		node.pos = [ 200, 200 ];
+
+        // insert position
+        /*let x, y;
+        x = node.panel.pos[0];
+        y = node.panel.pos[1];
+        for (let i in nodes) {
+            let panel = nodes[i].panel;
+            while (true) {
+                let f = true;
+                if (Math.abs(x - panel.pos[0]) < 50) {
+                    x += 50; f = false;
+                }
+                if (Math.abs(y - panel.pos[1]) < 50) {
+                    y += 50; f = false;
+                }
+                if (f) {
+                    break;
+                }
+            }
+        }
+        node.panel.pos = [x, y];*/
+
+		this.addNode(node);
+        
+        return true;
 	}
 
 	/**
