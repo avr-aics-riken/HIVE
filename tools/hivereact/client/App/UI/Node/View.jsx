25534f7be0758f62ea3c544f2f8abaa890697bb7
masahiro sugimoto <masahiro-s@digirea.com>
2016-02-26 21:40:07 +0900

    nodelist popup event target change

--- tools/hivereact/client/App/UI/Node/View.jsx
+++ tools/hivereact/client/App/UI/Node/View.jsx
@@ -16,11 +16,74 @@ var Dispatcher = require("flux").Dispatcher;
  */
 export default class View extends React.Component {
 	constructor(props) {
-		super(props);
+        super(props);
+
+        this.state = {
+            visible: false
+        };
+        this.listVisiblity = false;
+
 		//var dispatcher =  new Dispatcher();
 		this.nodeStore = new Store(this.props.action.dispatcher, this.props.store);
 		this.nodeAction = new Action(this.props.action.dispatcher, this.nodeStore.getDispatchToken());
-	}
+        this.dblClickEvent = this.dblClickEvent.bind(this);
+        this.keyDownEvent = this.keyDownEvent.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.generator = this.generator.bind(this);
+    }
+
+    dblClickEvent(eve){
+        this.listVisiblity = !this.listVisiblity;
+        this.setState({visible: this.listVisiblity});
+        if(this.listVisiblity){
+            setTimeout((()=>{
+                var e = ReactDOM.findDOMNode(this.refs.creator.refs.creator.refs.suggest.input);
+                e.focus();
+            }).bind(this), 100);
+        }
+    }
+
+    keyDownEvent(eve){
+        switch(eve.keyCode){
+            case 27:
+                this.listVisiblity = false;
+                this.setState({visible: false});
+                break;
+            case 32:
+                this.listVisiblity = true;
+                this.setState({visible: true});
+                setTimeout((()=>{
+                    var e = ReactDOM.findDOMNode(this.refs.creator.refs.creator.refs.suggest.input);
+                    e.focus();
+                }).bind(this), 100);
+                break;
+            default:
+                break;
+        }
+    }
+
+    componentDidMount(){
+        var e = this.refs.plugView.refs.svg;
+        e.addEventListener('dblclick', this.dblClickEvent.bind(this), true);
+        window.addEventListener('keydown', this.keyDownEvent.bind(this));
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('keydown', this.keyDownEvent.bind(this));
+    }
+
+    generator(){
+        if(this.state.visible){
+            return (
+                <NodeListCreate
+                    store={this.props.store}
+                    action={this.props.action}
+                    visibility={this.state.visible}
+                    ref="creator"
+                />
+            );
+        }
+    }
 
 	render () {
 		return (<div style={{position:"absolute",width:"100%",height:"100%"}}>
@@ -35,15 +98,13 @@ export default class View extends React.Component {
 						action={this.props.action}
 						nodeStore={this.nodeStore}
 						nodeAction={this.nodeAction}
+                        ref="plugView"
 					/>
 					<Property.View
 						store={this.props.store}
 						action={this.props.action}
 					/>
-					<NodeListCreate
-						store={this.props.store}
-						action={this.props.action}
-					/>
+                    {this.generator.bind(this)()}
 				</div>);
 	}
 }
