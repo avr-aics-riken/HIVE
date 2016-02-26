21cf6b972db7a0852d58b25290a0ddbfcbb3a903
KazumaHatta <tori31001@gmail.com>
2016-02-26 13:49:03 +0900

    fix node move slowdown

---
+++ tools/hivereact/client/App/UI/Node/Property/ItemView.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Core from '../../../Core'
+import ItemText from './ItemText.jsx'
+import ItemVec from './ItemVec.jsx'
+import ItemArray from './ItemArray.jsx'
+
+/**
+ * ノードプロパティ1ノード分.
+ */
+export default class ItemView extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	styles() {
+		return {
+			view : {
+				width : "200px",
+				height : "100%",
+				backgroundColor : "rgb(80, 80, 80)",
+				color : "white"
+			}
+		}
+	}
+
+	contents() {
+		return (
+			<div>
+				<ItemText />
+				<ItemVec />
+				<ItemArray />
+			</div>
+		);
+	}
+
+	render () {
+		const styles = this.styles.bind(this)();
+		return (<div style={styles.view}>
+			{this.contents.bind(this)()}
+		</div>);
+	}
+}
