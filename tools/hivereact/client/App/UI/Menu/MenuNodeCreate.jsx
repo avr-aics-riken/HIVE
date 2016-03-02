89311544f9ce68a0f96ce84ac61b1fe4c0593639
masahiro sugimoto <masahiro-s@digirea.com>
2016-03-02 16:33:36 +0900

    fix node creator style

--- tools/hivereact/client/App/UI/Menu/MenuNodeCreate.jsx
+++ tools/hivereact/client/App/UI/Menu/MenuNodeCreate.jsx
@@ -129,7 +129,7 @@ export default class MenuNodeCreate extends React.Component {
                 },
                 suggestionFocused: {
                     backgroundColor: "rgba(16, 16, 16, 1.0)",
-                    color: "red"
+                    color: "turquoise"
                 }
             }
         };
