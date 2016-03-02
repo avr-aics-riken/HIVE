7c3739ae08d271437f59a30471d469b45ebe7cca
kioku <kentaro-o@digirea.com>
2016-03-02 19:55:20 +0900

    add solver interface and sample

--- HiveCore/test/solversample.lua
+++ HiveCore/test/solversample.lua
@@ -1,1 +1,14 @@
-print('hoge')
+print('---This is solversample.lua---')
+
+local name = 'testdata'
+local data = getMemoryData(name)
+print(name, "=", data)
+if data then	
+	for i,v in pairs(data) do
+		print (i, v)
+	end
+end
+
+
+local img = ImageLoader()
+img:LoadRawFromPointer(1024,1024,4,8,data.pointer)
