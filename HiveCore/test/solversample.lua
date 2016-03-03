2f1945cebc22c722fed067a15c8fce62527607e2
kioku <kentaro-o@digirea.com>
2016-03-03 10:31:50 +0900

    add enum function

--- HiveCore/test/solversample.lua
+++ HiveCore/test/solversample.lua
@@ -1,7 +1,21 @@
 print('---This is solversample.lua---')
 
+local names = getMemoryDataNames()
+
+-- dump
+if names then
+	print('All memory data')
+	for i,v in pairs(names) do
+		print ('['..i..']=', v)
+	end
+end
+
+
+
 local name = 'testdata'
 local data = getMemoryData(name)
+
+-- dump
 print(name, "=", data)
 if data then	
 	for i,v in pairs(data) do
