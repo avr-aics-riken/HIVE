b795d46728c5b9b40b136b1c8b3201ae9c7d9adf
Syoyo Fujita <syoyo@lighttransport.com>
2016-03-05 18:33:44 +0900

    Add HIVESparseVolume.h header and RawSparseVolumeLoader class for In-situ visualization.

--- HiveCore/test/solversample.lua
+++ HiveCore/test/solversample.lua
@@ -1,3 +1,6 @@
+--
+-- Setup: Copy def_volume_raymarch_tf.frag from $HIVE/hrender/test
+--
 print('---This is solversample.lua---')
 
 local names = getMemoryDataNames()
@@ -12,7 +15,7 @@ end
 
 
 
-local name = 'testdata'
+local name = 'SparseVolumeScalar'
 local data = getMemoryData(name)
 
 -- dump
@@ -24,5 +27,50 @@ if data then
 end
 
 
-local img = ImageLoader()
-img:LoadRawFromPointer(1024,1024,4,8,data.pointer)
+local sparseVol = require("RawSparseVolumeLoader")()
+sparseVol:LoadFromPointer(data.pointer)
+
+local volume = SparseVolumeModel()
+volume:Create(sparseVol:SparseVolumeData())
+
+local camera = Camera()
+camera:SetScreenSize(1024, 1024)
+camera:SetFilename('image_solversample.jpg')
+
+camera:LookAt(
+   200,200,200,
+     0,0,0,
+     0,1,0,
+     60)
+
+function JetTransferFunctionTexture()
+   -- Create 1D transfer function texture
+   -- Jet color table from OSPray example
+   -- r, g, b, alpha
+   local tf = 
+        { 0         , 0           , 0.562493 , 1.0
+        , 0         , 0           , 1        , 1.0
+        , 0         , 1           , 1        , 1.0
+        , 0.500008  , 1           , 0.500008 , 1.0
+        , 1         , 1           , 0        , 1.0
+        , 1         , 0           , 0        , 1.0
+        , 0.500008  , 0           , 0        , 1.0 }
+
+   local width = #tf / 4
+   local tfTex = GenTexture()
+   tfTex:Create2D(tf, 3, width, 1); -- 3 = RGBA32F
+
+   return tfTex;
+end
+
+local tftex = JetTransferFunctionTexture()
+volume:SetShader('def_volume_raymarch_tf.frag') -- $HIVE/hrender/test
+volume:SetTexture('tf_tex', tftex:ImageData())
+volume:SetClampToEdge(true, true, true)
+volume:SetTextureWrapping('tf_tex', true, true, true) -- CLAMP_TO_EDGE wrap mode for transfer function texture.
+volume:SetFloat('tf_min', -10.5)
+volume:SetFloat('tf_max', 400.0)
+volume:SetFloat('tf_opacity', 0.0025)
+
+render {camera, volume}
+
