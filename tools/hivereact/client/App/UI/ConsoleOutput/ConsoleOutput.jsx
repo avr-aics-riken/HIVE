8e62946b6cd72e0b9f3ff3d9edfad72bd8898414
kioku <kentaro-o@digirea.com>
2016-03-08 22:12:52 +0900

    update consoleoutput

--- tools/hivereact/client/App/UI/ConsoleOutput/ConsoleOutput.jsx
+++ tools/hivereact/client/App/UI/ConsoleOutput/ConsoleOutput.jsx
@@ -4,20 +4,27 @@ import ReactDOM from 'react-dom'
 export default class ConsoleOutput extends React.Component {
     constructor(props) {
         super(props);
-        this.props = props;        
+        this.props = props;
+        
+        this.state = {
+            consoletext: 'This console output'
+        }        
+        
     }
+    
     styles(show) {
         return {
             position: "absolute",
-            backgroundColor: "#F00",
+            color: "#CCC",
+            backgroundColor: "rgba(50,50,50, 0.8)",
             width: "500px",  
             height: "500px",
             zIndex: "100",
             display: (show ? "block" : "none")
         };
     }
-    render () {
+    render () {        
         const style = this.styles(this.props.show);
-        return (<div style={style}>console</div>);
+        return (<div style={style}>{this.state.consoletext}</div>);
     }
 }
