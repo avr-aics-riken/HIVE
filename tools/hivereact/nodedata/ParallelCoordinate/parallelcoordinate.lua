559e2497fc7134ee619b5fb74564feb6c6ae8432
kioku <kentaro-o@digirea.com>
2016-03-09 20:52:35 +0900

    update parcoord

--- tools/hivereact/nodedata/ParallelCoordinate/parallelcoordinate.lua
+++ tools/hivereact/nodedata/ParallelCoordinate/parallelcoordinate.lua
@@ -9,12 +9,45 @@ end
 
 function ParallelCoordinate:Do()
     self:UpdateValue()
-    -- nothing to do
+
+    local vol = self.value.volume
+    print('vol=', vol)
+    if vol == nil then
+        return false
+    end
+    
+    local w = vol:Width()
+    local h = vol:Height()
+    local d = vol:Depth()    
+    local c = vol:Component()    
+    
+    if (w == 0 or h == 0 or d == 0) then
+        print('Size zero=', w, h, d)
+        return false
+    end
+    
+    local json = [[{
+        "JSONRPC" : "2.0",
+        "method" : "renderedImage",            
+        "to" : ]] .. targetClientId ..[[,
+        "param" : {
+        "type" : "volume",
+        "width" : "]] .. w .. [[",
+        "height" : "]] .. h .. [[",
+        "depth" : "]] .. d .. [[",
+        "component" : "]] .. c .. [[",
+        "varname": "]] .. self.varname .. [["
+        },
+        "id":0
+    }]]
+    
+    local imageBuffer = vol:Buffer()
+    local imageBufferSize = w * h * d * 4
+    HIVE_metabin:Create(json, imageBuffer, imageBufferSize)
+    network:SendBinary(HIVE_metabin:BinaryBuffer(), HIVE_metabin:BinaryBufferSize())
+    return true
 end
 
 function ParallelCoordinate:color()
-    self:UpdateValue()
-    local v = self.value
-    local value = v.value
-    return value
+    return {0,0,0,0}
 end
