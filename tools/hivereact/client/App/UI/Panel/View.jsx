ac7bee33e34f2f15e6913db750bb6959fe979c7a
KazumaHatta <tori31001@gmail.com>
2016-03-14 20:41:19 +0900

    change zoom max

--- tools/hivereact/client/App/UI/Panel/View.jsx
+++ tools/hivereact/client/App/UI/Panel/View.jsx
@@ -77,8 +77,8 @@ export default class View extends React.Component {
             zoom = zoom + mv;
             if (zoom <= 0.1) {
                 zoom = 0.1;
-            } else if (zoom >= 2.0) {
-                zoom = 2.0;
+            } else if (zoom >= 1.0) {
+                zoom = 1.0;
             }
 			this.setState({
 				zoom : zoom
