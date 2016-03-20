1e7ecc9f8c10c147ce3da371d1c2327837b4cdf9
kioku <kentaro-o@digirea.com>
2016-03-20 16:40:18 +0900

    update volume quantizer

--- lib/Builder/VolumeQuantizer/VolumeQuantizer.h
+++ lib/Builder/VolumeQuantizer/VolumeQuantizer.h
@@ -14,16 +14,23 @@ class VolumeQuantizer : public RefCount
 {
 private:
     RefPtr<BufferVolumeData> m_volume;
+    std::vector<RefPtr< BufferVolumeData> > m_inputs;
     
     std::vector<std::pair<float, float> > m_minmax;
     int m_quantizeSize;
+    int m_sampling[3];
     
 public:
     VolumeQuantizer();
-    int Create(BufferVolumeData *volume);
+    int Create();
+    bool Clear();
+    bool Add(BufferVolumeData *volume);
+
     BufferVolumeData*  VolumeData();
 
     bool QuantizeSize(int qsize);
+    bool SamplingNum(int widthDiv, int heightDiv, int depthDiv);
+
     const std::vector<std::pair<float, float> >& GetMinMax();
 };
 
