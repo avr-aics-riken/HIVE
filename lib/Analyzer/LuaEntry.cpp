b8129ebd122d7bff7787fe7cdb4463b16a1c90b5
Syoyo Fujita <syoyo@lighttransport.com>
2016-03-21 16:55:31 +0900

    Add SolidAnalyzer module.

--- lib/Analyzer/LuaEntry.cpp
+++ lib/Analyzer/LuaEntry.cpp
@@ -2,6 +2,7 @@
 #include "VolumeAnalyzer_Lua.h"
 #include "SparseVolumeAnalyzer_Lua.h"
 #include "PolygonAnalyzer_Lua.h"
+#include "SolidAnalyzer_Lua.h"
 
 extern "C" {
 
@@ -10,6 +11,7 @@ int luaopen_Analyzer(lua_State* L)
     LUA_SCRIPTCLASS_REGISTER(L, VolumeAnalyzer_Lua);
     LUA_SCRIPTCLASS_REGISTER(L, SparseVolumeAnalyzer_Lua);
     LUA_SCRIPTCLASS_REGISTER(L, PolygonAnalyzer_Lua);
+    LUA_SCRIPTCLASS_REGISTER(L, SolidAnalyzer_Lua);
     lua_newtable(L);
     lua_pushstring(L, "VolumeAnalyzer");
     lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(VolumeAnalyzer_Lua));
@@ -21,6 +23,10 @@ int luaopen_Analyzer(lua_State* L)
 
     lua_pushstring(L, "PolygonAnalyzer");
     lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(PolygonAnalyzer_Lua));
+    lua_settable(L, -3);
+
+    lua_pushstring(L, "SolidAnalyzer");
+    lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(SolidAnalyzer_Lua));
     lua_settable(L, -3);
 
     return 1;
