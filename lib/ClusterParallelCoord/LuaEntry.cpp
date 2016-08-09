80eaef50f39a1066cce113e78507ba08b2a8f19f
kioku <kentaro-o@digirea.com>
2016-08-09 21:10:36 +0900

    update ClusterParallelCoord

--- lib/ClusterParallelCoord/LuaEntry.cpp
+++ lib/ClusterParallelCoord/LuaEntry.cpp
@@ -3,11 +3,11 @@
 
 extern "C" {
 
-int luaopen_VolumeClusterAnalyzer(lua_State* L)
+int luaopen_ClusterParallelCoord(lua_State* L)
 {
     LUA_SCRIPTCLASS_REGISTER(L, VolumeClustering_Lua);
     lua_newtable(L);
-    lua_pushstring(L, "VolumeClusterAnalyzer");
+    lua_pushstring(L, "VolumeClustering");
     lua_pushcfunction(L, LUA_SCRIPTCLASS_NEW_FUNCTION(VolumeClustering_Lua));
     lua_settable(L, -3);
 
--- lib/ClusterParallelCoord/VolumeClustering.cpp
+++ lib/ClusterParallelCoord/VolumeClustering.cpp
@@ -5,6 +5,8 @@
 
 #include <string>
 #include "VolumeClustering.h"
+#include "BufferVolumeData.h"
+#include <math.h>
 
 /// コンストラクタ
 VolumeClustering::VolumeClustering()
@@ -35,8 +37,55 @@ bool VolumeClustering::Execute(BufferVolumeData* volume)
         return false;
     }
 
-    // TODO
+    const int pNum = 3;    
+    const int fNum = 1024;
+    float f[pNum][fNum];
+    for (int p = 0; p < pNum; ++p) {
+        for (int i = 0; i < fNum; ++i) {
+            f[p][i] = 0.0f;
+        }
+    }
 
+    const int w = volume->Width();
+    const int h = volume->Height();
+    const int d = volume->Depth();
+    const int c = volume->Component();
+    const float* buffer = static_cast<const float*>(volume->Buffer()->GetBuffer());
+    
+    printf("Volume Size = (%dx%dx%dx%d)\n", w, h, d, c); 
+
+    // get min/max
+    float minVal[pNum];
+    float maxVal[pNum];
+    for (int p = 0; p < pNum; ++p) {
+        minVal[p] =  MAXFLOAT;
+        maxVal[p] = -MAXFLOAT;        
+        for (int i = 0; i < w * h * d; ++i) {
+            const float v = buffer[c * i + p];
+            minVal[p] = fmin(v, minVal[p]);
+            maxVal[p] = fmax(v, maxVal[p]);                
+        }
+    }
+
+    for (int p = 0; p < pNum; ++p) {
+        printf("%d - Min = %f / Max = %f\n", p, minVal[p], maxVal[p]);
+    }
+
+    const float sigma = 10.0 / fNum;
+    for (int p = 0; p < pNum; ++p) {
+        const float minMaxDiffP = maxVal[p] - minVal[p];
+        for (int fn = 0; fn < fNum; ++fn) {
+            for (int i = 0; i < w * h * d; ++i) {
+                const float v = buffer[c * i + p];
+                const float fidx = static_cast<float>(fn)+0.5;
+                const float vi = (fidx / fNum) * minMaxDiffP + minVal[p];
+                f[p][fn] += exp( - 0.5 * ( ( (v - vi)/sigma )*( (v - vi)/sigma ) )  );
+            }
+        }
+    }
+    for (int i = 0; i < fNum; ++i) {
+        printf("%4d = %6.3f, %6.3f, %6.3f\n", i, f[0][i], f[1][i], f[2][i]);
+    }
     return true;
 }
 
