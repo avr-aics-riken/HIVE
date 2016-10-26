c03356d5ffb6955b9a0c816cf3f209a6ae4ab779
kioku <kentaro-o@digirea.com>
2016-10-26 12:18:00 +0900

    support STL Ascii format

--- lib/Loader/StlLoader/SimpleSTLB.h
+++ lib/Loader/StlLoader/SimpleSTLB.h
@@ -9,30 +9,28 @@
     SimpleSTLB
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <vector>
+#include "SimpleSTL.h"
+
 
 #if 0
 inline unsigned int swap4(const unsigned int* val)
 {
-	unsigned int tmp = *val;
-	unsigned char *dst = (unsigned char*)&tmp;
-	const unsigned char *src = (const unsigned char*)val;
-	
-	dst[0] = src[3];
-	dst[1] = src[2];
-	dst[2] = src[1];
-	dst[3] = src[0];
-	return tmp;
+    unsigned int tmp = *val;
+    unsigned char *dst = (unsigned char*)&tmp;
+    const unsigned char *src = (const unsigned char*)val;
+    
+    dst[0] = src[3];
+    dst[1] = src[2];
+    dst[2] = src[1];
+    dst[3] = src[0];
+    return tmp;
 }
 
 inline float swap4(float* f)
 {
-	unsigned int* d = reinterpret_cast<unsigned int*>(f);
-	unsigned int i = swap4(d);
-	return *reinterpret_cast<float*>(&i);
+    unsigned int* d = reinterpret_cast<unsigned int*>(f);
+    unsigned int i = swap4(d);
+    return *reinterpret_cast<float*>(&i);
 }
 #else
 inline float swap4(const unsigned int* i){ return *i; }
@@ -40,54 +38,17 @@ inline float swap4(float* f)       { return *f; }
 
 #endif
 
+
 /**
  * SimpleSTLB
  */
-class SimpleSTLB
+class SimpleSTLB : public SimpleSTL
 {
 public:
-	SimpleSTLB(){}
+    SimpleSTLB() : SimpleSTL() {}
 	~SimpleSTLB(){}
 	
-    float* GetPositionBuffer()
-    {
-        return &m_pos[0].x;
-    }
-    float* GetNormalBuffer()
-    {
-        return &m_normal[0].x;
-    }
-    
-    unsigned int* GetIndex()
-    {
-		if (m_face.size())
-			return &m_face[0].i0;
-		else
-			return 0;
-    }
-	
-    unsigned int GetVertexNum() const
-    {
-        return static_cast<unsigned int>(m_pos.size());
-    }
-    
-    unsigned int GetIndexNum() const
-    {
-        return static_cast<unsigned int>(m_face.size() * 3);
-    }
-
-    /**
-     * SimpleSTLB Triangle Info
-     */
-	struct triData
-	{
-		float normal[3];
-		float vtx0[3];
-		float vtx1[3];
-		float vtx2[3];
-		unsigned short pad;
-	};
-	bool Load(const char* filename, bool generateIndex = false)
+  	bool Load(const char* filename, bool generateIndex = false)
 	{
 		FILE* fp = fopen(filename, "rb");
 		if (!fp)
@@ -167,46 +128,6 @@ public:
 		return true;
 	}
 	
-	
-	
-private:
-	struct vec3{
-        float x,y,z;
-		vec3(){}
-		vec3(float x_, float y_, float z_) : x(x_),y(y_),z(z_){}
-        
-		vec3 operator-(const vec3& b) const
-        {
-            return vec3(this->x - b.x, this->y - b.y, this->z - b.z);
-        }
-        vec3 operator+(const vec3& b) const
-        {
-            return vec3(this->x + b.x, this->y + b.y, this->z + b.z);
-        }
-    };
-    vec3 cross(const vec3& a, const vec3& b) const
-    {
-        return vec3(a.y * b.z - a.z * b.y,
-                    a.z * b.x - a.x * b.z,
-                    a.x * b.y - a.y * b.x);
-    }
-    float length(const vec3& v) const
-    {
-        return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-    }
-    vec3 normalize(const vec3& v) const
-    {
-        const float l = length(v);
-        return vec3(v.x / l, v.y / l, v.z / l);
-    }
-	
-	struct Index{
-        unsigned int i0,i1,i2;
-    };
-	std::vector<vec3>  m_pos;
-    std::vector<vec3>  m_normal;
-    std::vector<Index> m_face;
-
 };
 
 #endif // INCLUDE_SIMPLESTLB
--- lib/Loader/StlLoader/StlLoader.cpp
+++ lib/Loader/StlLoader/StlLoader.cpp
@@ -6,7 +6,41 @@
 
 #include "StlLoader.h"
 #include "SimpleSTLB.h"
+#include "SimpleSTLA.h"
 #include "Buffer.h"
+
+
+namespace {
+    
+    bool isSTLBinaryFile(const char* filename, int maxlen = 400)
+    {
+        FILE* fp = fopen(filename, "rb");
+        if (!fp) {
+            return false;
+        }
+        unsigned char* buf = new unsigned char[maxlen];
+        memset(buf, 0, maxlen);
+        fread(buf,maxlen,1,fp);
+        fclose(fp);
+        
+        const char* stlascii_header = "solid";
+        
+        //space char escape
+        const char* data = reinterpret_cast<const char*>(buf) ;
+        int i = skipSpace(data);
+        
+        if (strncmp( &data[i], stlascii_header, strlen(stlascii_header)) == 0) {
+            const int testlen = maxlen/4; // test 1/4 size.
+            for (int i = 0; i < testlen; ++i) {
+                if (buf[i] >= 0x7F || buf[i] == 0x00)
+                    return true;
+            }
+            return false; // Ascii
+        }
+        return true;
+    }
+}
+
 
 /// コンストラクタ
 STLLoader::STLLoader()
@@ -32,24 +66,33 @@ void STLLoader::Clear()
  */
 bool STLLoader::Load(const char* filename){
 	Clear();
-	SimpleSTLB obj;
-	bool r = obj.Load(filename);
+    
+    bool r = false;
+    SimpleSTL* obj;
+    if (isSTLBinaryFile(filename)) {
+        obj = new SimpleSTLB();
+        r = obj->Load(filename);
+    } else {
+        obj = new SimpleSTLA();
+        r = obj->Load(filename);
+    }
 
-    mesh->Create(obj.GetVertexNum(), obj.GetIndexNum());
+    mesh->Create(obj->GetVertexNum(), obj->GetIndexNum());
     Vec3Buffer* pos = mesh->Position();
     Vec3Buffer* normal = mesh->Normal();
     FloatBuffer* mat = mesh->Material();
     UintBuffer* index = mesh->Index();
-    Vec2Buffer* texcoord = mesh->Texcoord();
+    //Vec2Buffer* texcoord = mesh->Texcoord();
 
-	memcpy(pos->GetBuffer(), obj.GetPositionBuffer(), sizeof(float) * 3 * pos->GetNum());
-	normal->Create(obj.GetVertexNum());
-	memcpy(normal->GetBuffer(), obj.GetNormalBuffer(), sizeof(float) * 3 * normal->GetNum());
-	mat->Create(obj.GetVertexNum());
+	memcpy(pos->GetBuffer(), obj->GetPositionBuffer(), sizeof(float) * 3 * pos->GetNum());
+	normal->Create(obj->GetVertexNum());
+	memcpy(normal->GetBuffer(), obj->GetNormalBuffer(), sizeof(float) * 3 * normal->GetNum());
+	mat->Create(obj->GetVertexNum());
 	memset(mat->GetBuffer(), 0, sizeof(float) * mat->GetNum());
-	index->Create(obj.GetIndexNum());
-	memcpy(index->GetBuffer(), obj.GetIndex(), sizeof(unsigned int) * index->GetNum());
+	index->Create(obj->GetIndexNum());
+	memcpy(index->GetBuffer(), obj->GetIndex(), sizeof(unsigned int) * index->GetNum());
 
+    delete obj;
     
 	printf("%d\n", pos->GetNum()     );
 	printf("%d\n", normal->GetNum()  );
