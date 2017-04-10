61fc107cf1d0e0d0e4fb15bcb2095fdaa689c9d2
Syoyo Fujita <syoyo@lighttransport.com>
2017-04-10 15:49:39 +0900

    Update K loader build script.

--- scripts/build_loader_libs_k_cross.sh
+++ scripts/build_loader_libs_k_cross.sh
@@ -32,29 +32,39 @@ function build_tp {
 	mkdir -p build
 	cd build
 
-	CXX=${cxx_compiler} CC=${c_compiler} CFLAGS="${c_flags}" CXXFLAGS=${cxx_flags} ../configure --host=sparc64-unknown-linux-gnu --prefix=${installdir}/TextParser && make && make install
+	CXX=${cxx_compiler} CC=${c_compiler} CFLAGS="${c_flags}" CXXFLAGS=${cxx_flags} ../configure --host=sparc64-unknown-linux-gnu --prefix=${installdir}/TextParser && make V=1 && make install
 	cd ${topdir}
 }
 
 function build_cdmlib {
+	echo "====> Building CDMlib..."
+
 	#
 	# CDMlib
 	#
 	cd third_party/
-	cd CDMlib/
+	#cd CDMlib/
 
-	# It looks like setting CXX/CC require regeneration of configure.
-	autoreconf -ivf
 
-	rm -rf build
-	mkdir -p build
-	cd build
+	#CXX=${cxx_compiler} CC=${c_compiler} CFLAGS="${c_flags}" CXXFLAGS="${cxx_flags} -I/opt/aics/netcdf/k/include" LDFLAGS="-L/opt/aics/netcdf/k/lib-static" LIBS="-lnetcdf -lhdf5_hl -lhdf5 -lsz -lz" F90=${f90_compiler} F90FLAGS=${f90_flags} ../configure --prefix=${installdir}/CDMlib --with-parser=${installdir}/TextParser --host=sparc64-unknown-linux-gnu --with-MPI=yes --with-nc && make && make install
 
-	CXX=${cxx_compiler} CC=${c_compiler} CFLAGS="${c_flags}" CXXFLAGS="${cxx_flags} -I/opt/aics/netcdf/k/include" LDFLAGS="-L/opt/aics/netcdf/k/lib-static" LIBS="-lnetcdf -lhdf5_hl -lhdf5 -lsz -lz" F90=${f90_compiler} F90FLAGS=${f90_flags} ../configure --prefix=${installdir}/CDMlib --with-parser=${installdir}/TextParser --host=sparc64-unknown-linux-gnu --with-MPI=yes --with-nc && make && make install
+	rm -rf CDMlib_build
+	mkdir -p CDMlib_build
+	cd CDMlib_build
+
+	#CXX=${cxx_compiler} CC=${c_compiler} CFLAGS=${c_flags} CXXFLAGS=${cxx_flags} ${CMAKE_BIN} -DINSTALL_DIR=${installdir}/CDMlib -Dwith_MPI=yes -Dwith_TP=${installdir}/TextParser -Dwith_NetCDF=${installdir} -Dwith_util=no -Dwith_example=no ../CDMlib && make && make install
+
+	CXX=${cxx_compiler} CC=${c_compiler} CFLAGS=${c_flags} CXXFLAGS=${cxx_flags} ${CMAKE_BIN} -DINSTALL_DIR=${installdir}/CDMlib -DCMAKE_TOOLCHAIN_FILE=../CDMlib/cmake/Toolchain_K.cmake -Dwith_MPI=yes -Dwith_example=yes -Dwith_util=no -Dwith_TP=${installdir}/TextParser -Dwith_CPM=${installdir}/CPMlib -Dwith_NetCDF=/opt/aics/netcdf/k -Denable_BUFFER_SIZE=no ../CDMlib && make VERVOSE=1 && make install
+
+	if [[ $? != 0 ]]; then exit $?; fi
 	cd ${topdir}
+
+	echo "====> Built CDMlib."
 }
 
 function build_polylib {
+	echo "====> Building Polylib..."
+
 	#
 	# Polylib
 	#
@@ -65,7 +75,7 @@ function build_polylib {
 	fi
 
 	# It looks like setting CXX/CC require regeneration of configure.
-	autoreconf -ivf
+	#autoreconf -ivf
 
 	rm -rf build
 	mkdir -p build
@@ -73,9 +83,13 @@ function build_polylib {
 
 	CXX=${cxx_compiler} CC=${c_compiler} CFLAGS="${c_flags}" CXXFLAGS="-Kfast,ocl,preex,simd=2,uxsimd,array_private,parallel,openmp" ../configure --prefix=${installdir}/Polylib --with-parser=${installdir}/TextParser --host=sparc64-unknown-linux-gnu && make && make install
 	cd ${topdir}
+
+	echo "====> Built Polylib."
 }
  
 function build_bcmtools {
+	echo "====> Building BCMTools..."
+
 	#
 	# BCMTools
 	#
@@ -87,9 +101,13 @@ function build_bcmtools {
 	autoreconf -ivf
 	CXX=${cxx_compiler} CC=${c_compiler} CXXFLAGS=${cxx_flags} CFLAGS="${c_flags}" ./configure --prefix=${installdir}/BCMTools --with-parser=${installdir}/TextParser --with-polylib=${installdir}/Polylib --host=sparc64-unknown-linux-gnu && make && make install
 	cd ${topdir}
+
+	echo "====> Built BCMTools."
 }
 
 function build_hdmlib {
+	echo "====> Building HDMlib..."
+
 	#
 	# HDMlib
 	#
@@ -104,9 +122,13 @@ function build_hdmlib {
 
 	CXX=${cxx_compiler} CC=${c_compiler} CXXFLAGS=${cxx_flags} CFLAGS=${c_flags} ../configure --prefix=${installdir}/HDMlib --with-parser=${installdir}/TextParser --with-bcm=${installdir}/BCMTools --host=sparc64-unknown-linux-gnu && make && make install
 	cd ${topdir}
+
+	echo "====> Built HDMlib."
 }
 
 function build_pdmlib {
+	echo "====> Building PDMlib..."
+
 	cd third_party/
 	rm -rf fpzip-1.0.1/
 	tar -zxvf fpzip-1.0.1.tar.gz
@@ -139,9 +161,12 @@ function build_pdmlib {
 	cd PDMlib_build/
 	CXX=${cxx_compiler} CC=${c_compiler} ${CMAKE_BIN} -DCMAKE_C_FLAGS=${c_flags} -DCMAKE_CXX_FLAGS=${cxx_flags} -DCMAKE_CXX_COMPILER=${cxx_compiler} -DCMAKE_C_COMPILER=${c_compiler} -DTP_ROOT=${installdir}/TextParser -DFPZIP_ROOT=${installdir} -DZOLTAN_ROOT=${installdir} -DCMAKE_INSTALL_PREFIX=${installdir}/PDMlib -Dbuild_h5part_converter=no -Dbuild_fv_converter=no -Dbuild_vtk_converter=no ../PDMlib && make && make install
 	cd ${topdir}
+
+	echo "====> Built PDMlib."
 }
 
 function build_udmlib {
+	echo "====> Building UDMlib..."
 	cd third_party/
 	rm -rf Zoltan_v3.81/
 	rm -rf Zoltan_build/
@@ -174,22 +199,34 @@ function build_udmlib {
 	CXX=${cxx_compiler} CC=${cxx_compiler} CXX_FLAGS=${cxx_flags} CFLAGS=${c_flags} ../configure --prefix=${installdir}/UDMlib --with-comp=FJ --host=sparc64-unknown-linux-gnu --with-tp=${installdir}/TextParser --with-zoltan=${installdir} --with-cgns=${installdir} && make && make install
 	if [[ $? != 0 ]]; then exit $?; fi
 	cd ${topdir}
+
+	echo "====> Built UDMlib."
 }
 
 function build_compositor {
 
+	echo "====> Building 234Compositor..."
+
 	cd third_party/ 
 	cd 234Compositor/
+
 	if [ -f "Makefile" ]; then
 		make distclean
 	fi
 
 	autoreconf -ivf
+
 	CXX=${cxx_compiler} CC=${c_compiler} CXXFLAGS="${cxx_flags} -Kopenmp" CFLAGS="${c_flags} -Kopenmp" ./configure --prefix=${installdir}/234Compositor --host=sparc64-unknown-linux-gnu && make && make install
+	if [[ $? != 0 ]]; then exit $?; fi
+
 	cd ${topdir}
+
+	echo "====> Built 234Compositor."
 }
 
 function build_nanomsg {
+
+	echo "====> Building nanomsg..."
 
 	cd third_party/ 
 	cd nanomsg/
@@ -200,9 +237,13 @@ function build_nanomsg {
 	autoreconf -ivf
 	CXX=${cxx_compiler} CC=${c_compiler} CXXFLAGS=${cxx_flags} CFLAGS=${c_flags} ./configure --prefix=${installdir}/nanomsg && make && make install
 	cd ${topdir}
+
+	echo "====> Built nanomsg."
 }
 
 function build_pmlib {
+
+	echo "====> Building PMlib..."
 
         cd third_party/PMlib
         autoreconf -ivf
@@ -213,10 +254,30 @@ function build_pmlib {
         CXX=${cxx_compiler} CC=${c_compiler} CXXFLAGS="${cxx_flags} -Kopenmp -Ntl_notrt -Nfjcex -DK_COMPUTER -W" CFLAGS="${c_flags} -Kopenmp -Ntl_notrt  -Nfjcex -DL_COMPUTER -W" ../configure --prefix=${installdir}/PMlib --with-comp="FJ" LDFLAGS="-L/opt/FJSVXosDevkit/sparc64fx/V01L02E07/target/usr/lib64" --host=sparc64-unknown-linux-gnu && make && make install
         if [[ $? != 0 ]]; then exit $?; fi
         cd ${topdir}
+
+	echo "====> Built PMlib."
+}
+
+function build_cpmlib {
+	echo "====> Building CPMlib...."
+
+        cd third_party/
+
+	rm -rf CPMlib_build
+	mkdir CPMlib_build
+	cd CPMlib_build
+
+	cmake -DINSTALL_DIR=${installdir}/CPMlib -DCMAKE_TOOLCHAIN_FILE=../CPMlib/cmake/Toolchain_K.cmake -Dwith_MPI=yes -Dreal_type=float -Denable_LMR=no -Dwith_example=no -Dwith_TP=${installdir}/TextParser ../CPMlib && make && make install
+        if [[ $? != 0 ]]; then exit $?; fi
+
+        cd ${topdir}
+
+	echo "====> Built CPMlib."
 }
 
 clean_install_dir
 build_tp
+build_cpmlib
 build_cdmlib
 build_polylib
 build_bcmtools
@@ -225,4 +286,4 @@ build_pdmlib
 build_udmlib
 build_compositor
 build_pmlib
-# build_nanomsg
+build_nanomsg
