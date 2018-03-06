23e305d6fbda0d44f5d74122da67b428961ca76c
KazumaHatta <tori31001@gmail.com>
2018-03-06 12:18:25 +0900

    change bbox to linemodel

--- HIVE/ModuleSystem/moduledata/Render/RenderView/renderview.lua
+++ HIVE/ModuleSystem/moduledata/Render/RenderView/renderview.lua
@@ -19,12 +19,13 @@ RenderView.new = function (varname)
         ipcmode = false
     }
     this.network_ipc = nil
+    this.gen = PrimitiveGenerator()
     
     setmetatable(this, {__index=RenderView})
     return this
 end
 
-function lineVertex(vlist, v1, v2)
+function insertLineVertex(vlist, v1, v2)
     for k, v in ipairs(v1) do
         table.insert(vlist, v);
     end
@@ -38,13 +39,46 @@ function transform(v, trans)
         trans[1] * v[1] + trans[5] * v[2] + trans[9]  * v[3] + trans[13] * 1.0,
         trans[2] * v[1] + trans[6] * v[2] + trans[10] * v[3] + trans[14] * 1.0,
         trans[3] * v[1] + trans[7] * v[2] + trans[11] * v[3] + trans[15] * 1.0,
-        trans[4] * v[1] + trans[8] * v[2] + trans[12] * v[3] + trans[16] * 1.0
+        --trans[4] * v[1] + trans[8] * v[2] + trans[12] * v[3] + trans[16] * 1.0
     };
 end
 
 function screenSpaceLineVertex(cam, vlist, v1, v2)
     table.insert(vlist, cam:ConvertToScreenSpace(v1[1], v1[2], v1[3]));
     table.insert(vlist, cam:ConvertToScreenSpace(v2[1], v2[2], v2[3]));
+end
+
+function getBBoxVerts(bmin, bmax)
+    local res = {}
+    local p1 = { bmin[1], bmax[2], bmin[3] };
+    local p2 = { bmax[1], bmax[2], bmin[3] };
+    local p3 = { bmax[1], bmin[2], bmin[3] };
+    local p4 = { bmax[1], bmin[2], bmax[3] };
+    local p5 = { bmin[1], bmin[2], bmax[3] };
+    local p6 = { bmin[1], bmax[2], bmax[3] };
+    table.insert(res, p1);
+    table.insert(res, p2);
+    table.insert(res, p3);
+    table.insert(res, p4);
+    table.insert(res, p5);
+    table.insert(res, p6);
+    table.insert(res, bmin);
+    table.insert(res, bmax);
+    return res;
+end
+
+function getBBoxMinMax(verts)
+    local bmin = {
+        math.min(verts[1][1], verts[2][1], verts[3][1], verts[4][1], verts[5][1], verts[6][1], verts[7][1], verts[8][1]),
+        math.min(verts[1][2], verts[2][2], verts[3][2], verts[4][2], verts[5][2], verts[6][2], verts[7][2], verts[8][2]),
+        math.min(verts[1][3], verts[2][3], verts[3][3], verts[4][3], verts[5][3], verts[6][3], verts[7][3], verts[8][3]),
+    };
+    local bmax = {
+        math.max(verts[1][1], verts[2][1], verts[3][1], verts[4][1], verts[5][1], verts[6][1], verts[7][1], verts[8][1]),
+        math.max(verts[1][2], verts[2][2], verts[3][2], verts[4][2], verts[5][2], verts[6][2], verts[7][2], verts[8][2]),
+        math.max(verts[1][3], verts[2][3], verts[3][3], verts[4][3], verts[5][3], verts[6][3], verts[7][3], verts[8][3]),
+    };
+    return {bmin, bmax}
 end
 
 function RenderView:Do()
@@ -67,6 +101,50 @@ function RenderView:Do()
 -- For Object
     if v.RenderObject then
         temp = v.RenderObject
+    end
+-- For bbox
+    if v.Camera then
+        targetcam = v.Camera
+    else
+        targetcam = self.cam
+    end
+    
+    local bbox_verts2D = {}
+    if (v.showbbox == true and v.RenderObject) then
+        local line_verts = {}
+        local shadername = '../shader/const.frag';
+        for i, t in ipairs(v.RenderObject) do
+            local bbox = t:GetBBox();
+            if bbox[1] ~= nil and bbox[2] ~= nil then
+                local verts = getBBoxVerts(bbox[1], bbox[2]);
+                local trans = t:GetTransformMatrix();
+                local bverts = {};
+                for k, v in ipairs(verts) do
+                    table.insert(bverts, transform(v, trans));
+                end
+                --local bboxTransed = getBBoxMinMax(bverts);
+                --bverts = getBBoxVerts(bboxTransed[1], bboxTransed[2]);
+                insertLineVertex(line_verts, bverts[7], bverts[1]);
+                insertLineVertex(line_verts, bverts[1], bverts[2]);
+                insertLineVertex(line_verts, bverts[2], bverts[3]);
+                insertLineVertex(line_verts, bverts[3], bverts[7]);
+                insertLineVertex(line_verts, bverts[7], bverts[5]);
+                insertLineVertex(line_verts, bverts[1], bverts[6]);
+                insertLineVertex(line_verts, bverts[2], bverts[8]);
+                insertLineVertex(line_verts, bverts[3], bverts[4]);
+                insertLineVertex(line_verts, bverts[8], bverts[4]);
+                insertLineVertex(line_verts, bverts[4], bverts[5]);
+                insertLineVertex(line_verts, bverts[5], bverts[6]);
+                insertLineVertex(line_verts, bverts[6], bverts[8]);
+            end
+        end
+        local line = self.gen:LineList(line_verts, #line_verts / 3, 1.0);
+        local linemodel = LineModel();
+        linemodel:Create(line)
+        linemodel:SetShader(shadername)
+        linemodel:SetVec4("color", v.bboxcolor[1], v.bboxcolor[2], v.bboxcolor[3], v.bboxcolor[4]);
+        linemodel:SetLineWidth(1)
+        temp[#temp + 1] = linemodel
     end
         
 -- For Camera
@@ -97,53 +175,6 @@ function RenderView:Do()
         render(temp)
     --end
     
--- For bbox
-    if v.Camera then
-        targetcam = v.Camera
-    else
-        targetcam = self.cam
-    end
-    
-    local bbox_verts2D = {}
-    if (v.showbbox == true and v.RenderObject) then
-        for i, t in ipairs(v.RenderObject) do
-            local linemodel = LineModel();
-            local shadername = '../shader/const.frag';
-            linemodel:SetShader(shadername)
-            local bbox = t:GetBBox();
-            if bbox[1] ~= nil and bbox[2] ~= nil then
-                local bmin = bbox[1];
-                local bmax = bbox[2];
-                local p1 = { bmin[1], bmax[2], bmin[3] };
-                local p2 = { bmax[1], bmax[2], bmin[3] };
-                local p3 = { bmax[1], bmin[2], bmin[3] };
-                local p4 = { bmax[1], bmin[2], bmax[3] };
-                local p5 = { bmin[1], bmin[2], bmax[3] };
-                local p6 = { bmin[1], bmax[2], bmax[3] };
-                local trans = t:GetTransformMatrix();
-                bmin = transform(bbox[1], trans);
-                bmax = transform(bbox[2], trans);
-                p1 = transform(p1, trans);
-                p2 = transform(p2, trans);
-                p3 = transform(p3, trans);
-                p4 = transform(p4, trans);
-                p5 = transform(p5, trans);
-                p6 = transform(p6, trans);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, bmin, p1);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p1, p2);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p2, p3);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p3, bmin);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, bmin, p5);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p1, p6);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p2, bmax);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p3, p4);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, bmax, p4);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p4, p5);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p5, p6);
-                screenSpaceLineVertex(targetcam, bbox_verts2D, p6, bmax);
-            end
-        end
-    end
     
     if network == nil and HIVE_metabin == nil then -- no UI mode        
         return true
